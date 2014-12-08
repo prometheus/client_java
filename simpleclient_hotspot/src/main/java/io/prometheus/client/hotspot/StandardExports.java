6757db31e4ddf018847f97e1201acd3cdf47843b
Brian Brazil <brian.brazil@boxever.com>
2014-12-08 15:18:41 +0000

    Add standard metrics via hotspot.

---
+++ simpleclient_hotspot/src/main/java/io/prometheus/client/hotspot/StandardExports.java
@@ -0,0 +1,132 @@
+package io.prometheus.client.hotspot;
+
+import com.sun.management.OperatingSystemMXBean;
+import com.sun.management.UnixOperatingSystemMXBean;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.lang.management.RuntimeMXBean;
+import java.lang.management.ManagementFactory;
+import java.util.logging.Logger;
+import java.util.Vector;
+
+import io.prometheus.client.Collector;
+
+/**
+ * Exports the standard exports common across all prometheus clients.
+ * <p>
+ * This includes stats like CPU time spent and memory usage.
+ * <p>
+ * Example usage:
+ * <pre>
+ * {@code
+ *   new StandardExports().register();
+ * }
+ * </pre>
+ */
+public class StandardExports extends Collector {
+  private static final Logger LOGGER = Logger.getLogger(StandardExports.class.getName());
+
+  private StatusReader statusReader;
+  private OperatingSystemMXBean osBean;
+  private RuntimeMXBean runtimeBean;
+  private boolean unix;
+  private boolean linux;
+
+  public StandardExports() {
+    this(new StatusReader(),
+         (OperatingSystemMXBean)ManagementFactory.getOperatingSystemMXBean(),
+         ManagementFactory.getRuntimeMXBean());
+  }
+
+  StandardExports(StatusReader statusReader, OperatingSystemMXBean osBean, RuntimeMXBean runtimeBean) {
+      this.statusReader = statusReader;
+      this.osBean = osBean;
+      this.runtimeBean = runtimeBean;
+      if (osBean instanceof UnixOperatingSystemMXBean) {
+        unix = true;
+      }
+      if (osBean.getName().indexOf("Linux") == 0) {
+        linux = true;
+      }
+  }
+
+  MetricFamilySamples singleMetric(String name, Type type, String help, double value) {
+    Vector<MetricFamilySamples.Sample> samples = new Vector<MetricFamilySamples.Sample>();
+    samples.add(new MetricFamilySamples.Sample(name, new String[]{}, new Vector<String>(), value));
+    return new MetricFamilySamples(name, type, help, samples);
+  }
+
+  private final static double NANOSECONDS_PER_SECOND = 1E9;
+  private final static double MILLISECONDS_PER_SECOND = 1E3;
+  private final static double KB = 1024;
+
+  public MetricFamilySamples[] collect() {
+    Vector<MetricFamilySamples> mfs = new Vector<MetricFamilySamples>();
+
+    mfs.add(singleMetric("process_cpu_seconds_total", Type.COUNTER, "CPU time used by the process in seconds.",
+        osBean.getProcessCpuTime() / NANOSECONDS_PER_SECOND));
+
+    mfs.add(singleMetric("process_start_time_seconds", Type.GAUGE, "Start time of the process, in unixtime.",
+        runtimeBean.getStartTime() / MILLISECONDS_PER_SECOND));
+
+    if (unix) {
+      UnixOperatingSystemMXBean unixBean = (UnixOperatingSystemMXBean) osBean;
+      mfs.add(singleMetric("process_open_fds", Type.GAUGE, "The number of open file descriptors.",
+          unixBean.getOpenFileDescriptorCount()));
+      mfs.add(singleMetric("process_max_fds", Type.GAUGE, "The maximum number of open file descriptors.",
+          unixBean.getMaxFileDescriptorCount()));
+    }
+
+    // There's no standard Java or POSIX way to get memory stats,
+    // so add support for just Linux for now.
+    if (linux) {
+      try {
+      collectMemoryMetricsLinux(mfs);
+      } catch (Exception e) {
+        // If the format changes, log a warning and return what we can.
+        LOGGER.warning(e.toString());
+      }
+    }
+
+    return mfs.toArray(new MetricFamilySamples[]{});
+  }
+
+  void collectMemoryMetricsLinux(Vector<MetricFamilySamples> mfs) {
+    // statm/stat report in pages, and it's non-trivial to get pagesize from Java
+    // so we parse status instead.
+    BufferedReader br = null;
+    try {
+      br = statusReader.procSelfStatusReader();
+      String line;
+      while ((line = br.readLine()) != null) {
+        if (line.startsWith("VmSize:")) {
+          mfs.add(singleMetric("process_virtual_memory_bytes", Type.GAUGE,
+              "The virtual memory size of the process, in bytes.",
+              Float.parseFloat(line.split(" +")[1]) * KB));
+        } else if (line.startsWith("VmRSS:")) {
+          mfs.add(singleMetric("process_resident_memory_bytes", Type.GAUGE,
+              "The resident memory size of the process, in bytes.",
+              Float.parseFloat(line.split(" +")[1]) * KB));
+        }
+      }
+    } catch (IOException e) {
+      LOGGER.fine(e.toString());
+    } finally {
+      if (br != null) {
+        try {
+          br.close();
+        } catch (IOException e) {
+          LOGGER.fine(e.toString());
+        }
+      }
+    }
+  }
+
+  static class StatusReader {
+    BufferedReader procSelfStatusReader() throws FileNotFoundException {
+      return new BufferedReader(new FileReader("/proc/self/status"));
+    }
+  }
+}
---
+++ simpleclient_hotspot/src/test/java/io/prometheus/client/hotspot/StandardExportsTest.java
@@ -0,0 +1,74 @@
+package io.prometheus.client.hotspot;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import com.sun.management.UnixOperatingSystemMXBean;
+import java.io.BufferedReader;
+import java.io.StringReader;
+import java.io.FileNotFoundException;
+import java.util.Vector;
+import java.lang.management.RuntimeMXBean;
+
+import io.prometheus.client.CollectorRegistry;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+public class StandardExportsTest {
+
+  static class StatusReaderTest extends StandardExports.StatusReader {
+    BufferedReader procSelfStatusReader() throws FileNotFoundException {
+      return new BufferedReader(new StringReader("Name:   cat\nVmSize:     5900 kB\nVmRSS:       360 kB\n"));
+    }
+  }
+
+  UnixOperatingSystemMXBean osBean;
+  RuntimeMXBean runtimeBean;
+
+  @Before
+  public void setUp() {
+    osBean = Mockito.mock(UnixOperatingSystemMXBean.class);
+    Mockito.when(osBean.getName()).thenReturn("Linux");
+    Mockito.when(osBean.getProcessCpuTime()).thenReturn(123L);
+    Mockito.when(osBean.getOpenFileDescriptorCount()).thenReturn(10L);
+    Mockito.when(osBean.getMaxFileDescriptorCount()).thenReturn(20L);
+    runtimeBean = Mockito.mock(RuntimeMXBean.class);
+    Mockito.when(runtimeBean.getStartTime()).thenReturn(456L);
+  }
+
+  @Test
+  public void testStandardExports() {
+    CollectorRegistry registry = new CollectorRegistry();
+    new StandardExports(new StatusReaderTest(), osBean, runtimeBean).register(registry);
+
+    assertEquals(123 / 1.0E9,
+        registry.getSampleValue("process_cpu_seconds_total", new String[]{}, new String[]{}), .0000001);
+    assertEquals(10,
+        registry.getSampleValue("process_open_fds", new String[]{}, new String[]{}), .001);
+    assertEquals(20,
+        registry.getSampleValue("process_max_fds", new String[]{}, new String[]{}), .001);
+    assertEquals(456 / 1.0E3,
+        registry.getSampleValue("process_start_time_seconds", new String[]{}, new String[]{}), .0000001);
+    assertEquals(5900 * 1024,
+        registry.getSampleValue("process_virtual_memory_bytes", new String[]{}, new String[]{}), .001);
+    assertEquals(360 * 1024,
+        registry.getSampleValue("process_resident_memory_bytes", new String[]{}, new String[]{}), .001);
+  }
+
+  @Test
+  public void testBrokenProcStatusReturnsOtherStats() {
+    class StatusReaderBroken extends StandardExports.StatusReader {
+      BufferedReader procSelfStatusReader() throws FileNotFoundException {
+        return new BufferedReader(new StringReader("Name:   cat\nVmSize:\n"));
+      }
+    }
+
+    CollectorRegistry registry = new CollectorRegistry();
+    new StandardExports(new StatusReaderBroken(), osBean, runtimeBean).register(registry);
+
+    assertEquals(123 / 1.0E9,
+      registry.getSampleValue("process_cpu_seconds_total", new String[]{}, new String[]{}), .0000001);
+    assertNull(registry.getSampleValue("process_resident_memory_bytes", new String[]{}, new String[]{}));
+  }
+}
