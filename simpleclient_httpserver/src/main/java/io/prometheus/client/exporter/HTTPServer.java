659645d0457bd7f8c49fc2012d0bb7f4d21d3675
Tamás Gulácsi <tgulacsi78@gmail.com>
2015-05-22 14:20:44 +0200

    use spaces instead of tabs

--- simpleclient_httpserver/src/main/java/io/prometheus/client/exporter/HTTPServer.java
+++ simpleclient_httpserver/src/main/java/io/prometheus/client/exporter/HTTPServer.java
@@ -15,39 +15,39 @@ import com.sun.net.httpserver.HttpServer;
 import com.sun.net.httpserver.HttpExchange;
 
 public class HTTPServer {
-	static class HTTPMetricHandler implements HttpHandler {
-		public void handle(HttpExchange t) throws IOException {
-			ByteArrayOutputStream response = dump();
-			t.getResponseHeaders().set("Content-Type",
-					TextFormat.CONTENT_TYPE_004);
-			t.getResponseHeaders().set("Content-Length",
-					String.valueOf(response.size()));
-			t.sendResponseHeaders(200, response.size());
-			response.writeTo(t.getResponseBody());
-			t.close();
-		}
+    static class HTTPMetricHandler implements HttpHandler {
+        public void handle(HttpExchange t) throws IOException {
+            ByteArrayOutputStream response = dump();
+            t.getResponseHeaders().set("Content-Type",
+                    TextFormat.CONTENT_TYPE_004);
+            t.getResponseHeaders().set("Content-Length",
+                    String.valueOf(response.size()));
+            t.sendResponseHeaders(200, response.size());
+            response.writeTo(t.getResponseBody());
+            t.close();
+        }
 
-	}
+    }
 
-	public static ByteArrayOutputStream dump() throws java.io.IOException {
-		ByteArrayOutputStream response = new ByteArrayOutputStream(1 << 20);
-		OutputStreamWriter osw = new OutputStreamWriter(response);
-		TextFormat.write004(osw,
-				CollectorRegistry.defaultRegistry.metricFamilySamples());
-		osw.flush();
-		osw.close();
-		response.flush();
-		response.close();
-		return response;
-	}
+    public static ByteArrayOutputStream dump() throws java.io.IOException {
+        ByteArrayOutputStream response = new ByteArrayOutputStream(1 << 20);
+        OutputStreamWriter osw = new OutputStreamWriter(response);
+        TextFormat.write004(osw,
+                CollectorRegistry.defaultRegistry.metricFamilySamples());
+        osw.flush();
+        osw.close();
+        response.flush();
+        response.close();
+        return response;
+    }
 
-	public HTTPServer(int port) throws java.io.IOException {
-		HttpServer mServer = HttpServer.create();
-		mServer.bind(new java.net.InetSocketAddress(port), 3);
-		HttpHandler mHandler = new HTTPMetricHandler();
-		mServer.createContext("/", mHandler);
-		mServer.createContext("/metrics", mHandler);
-		mServer.setExecutor(Executors.newSingleThreadExecutor());
-		mServer.start();
-	}
+    public HTTPServer(int port) throws java.io.IOException {
+        HttpServer mServer = HttpServer.create();
+        mServer.bind(new java.net.InetSocketAddress(port), 3);
+        HttpHandler mHandler = new HTTPMetricHandler();
+        mServer.createContext("/", mHandler);
+        mServer.createContext("/metrics", mHandler);
+        mServer.setExecutor(Executors.newSingleThreadExecutor());
+        mServer.start();
+    }
 }
