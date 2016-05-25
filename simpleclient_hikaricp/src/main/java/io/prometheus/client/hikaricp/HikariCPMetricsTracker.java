1364a11466a10ce63d2d32b1cd5d5dd5ad23a669
Tokuhiro Matsuno <tokuhirom@gmail.com>
2016-05-26 00:34:52 +0900

    Collect hikaricp_connection_acquired_nanos and hikaricp_connection_usage_millis in a summary

--- simpleclient_hikaricp/src/main/java/io/prometheus/client/hikaricp/HikariCPMetricsTracker.java
+++ simpleclient_hikaricp/src/main/java/io/prometheus/client/hikaricp/HikariCPMetricsTracker.java
@@ -2,10 +2,12 @@ package io.prometheus.client.hikaricp;
 
 import com.zaxxer.hikari.metrics.MetricsTracker;
 import io.prometheus.client.Counter;
-import io.prometheus.client.Gauge;
+import io.prometheus.client.Summary;
 
 public class HikariCPMetricsTracker extends MetricsTracker {
   private final Counter.Child counter;
+  private final Summary.Child elapsedAcquiredSummary;
+  private final Summary.Child elapsedBorrowedSummary;
 
   public HikariCPMetricsTracker(String poolName) {
     super();
@@ -17,21 +19,34 @@ public class HikariCPMetricsTracker extends MetricsTracker {
             .register();
 
     this.counter = counter.labels(poolName);
+
+    Summary elapsedAcquiredSummary = Summary.build()
+            .name("hikaricp_connection_acquired_nanos")
+            .labelNames("pool")
+            .help("Connection acquired time")
+            .register();
+    this.elapsedAcquiredSummary = elapsedAcquiredSummary.labels(poolName);
+
+    Summary elapsedBorrowedSummary = Summary.build()
+            .name("hikaricp_connection_usage_millis")
+            .labelNames("pool")
+            .help("Connection usage")
+            .register();
+    this.elapsedBorrowedSummary = elapsedBorrowedSummary.labels(poolName);
   }
 
   @Override
   public void recordConnectionAcquiredNanos(long elapsedAcquiredNanos) {
-    super.recordConnectionAcquiredNanos(elapsedAcquiredNanos);
+    elapsedAcquiredSummary.observe(elapsedAcquiredNanos);
   }
 
   @Override
   public void recordConnectionUsageMillis(long elapsedBorrowedMillis) {
-    super.recordConnectionUsageMillis(elapsedBorrowedMillis);
+    elapsedBorrowedSummary.observe(elapsedBorrowedMillis);
   }
 
   @Override
   public void recordConnectionTimeout() {
-    super.recordConnectionTimeout();
     counter.inc();
   }
 }
--- simpleclient_hikaricp/src/test/java/io/prometheus/client/hikaricp/HikariCPMetricsTrackerTest.java
+++ simpleclient_hikaricp/src/test/java/io/prometheus/client/hikaricp/HikariCPMetricsTrackerTest.java
@@ -2,13 +2,20 @@ package io.prometheus.client.hikaricp;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import io.prometheus.client.Collector;
 import io.prometheus.client.CollectorRegistry;
 import org.junit.Test;
 
+import java.sql.Connection;
 import java.sql.SQLTransientConnectionException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Enumeration;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.*;
+
 
 public class HikariCPMetricsTrackerTest {
   @Test
@@ -26,17 +33,38 @@ public class HikariCPMetricsTrackerTest {
     String[] labelValues = {poolName};
 
     HikariDataSource hikariDataSource = new HikariDataSource(config);
-    hikariDataSource.getConnection();
+    Connection connection = hikariDataSource.getConnection();
     try {
       hikariDataSource.getConnection();
     } catch (SQLTransientConnectionException e) {
     }
+    connection.close();
 
     assertThat(CollectorRegistry.defaultRegistry.getSampleValue(
             "hikaricp_connection_timeout_count",
             labelNames,
             labelValues)
             .doubleValue(), is(1.0));
+    assertThat(CollectorRegistry.defaultRegistry.getSampleValue(
+            "hikaricp_connection_acquired_nanos_count",
+            labelNames,
+            labelValues)
+            .doubleValue(), is(equalTo(1.0)));
+    assertThat(CollectorRegistry.defaultRegistry.getSampleValue(
+            "hikaricp_connection_acquired_nanos_sum",
+            labelNames,
+            labelValues)
+            .doubleValue(), is(greaterThan(0.0)));
+    assertThat(CollectorRegistry.defaultRegistry.getSampleValue(
+            "hikaricp_connection_usage_millis_count",
+            labelNames,
+            labelValues)
+            .doubleValue(), is(equalTo(1.0)));
+    assertThat(CollectorRegistry.defaultRegistry.getSampleValue(
+            "hikaricp_connection_usage_millis_sum",
+            labelNames,
+            labelValues)
+            .doubleValue(), is(greaterThan(0.0)));
   }
 
 }
