40564fe55f83294bccd995eeb056a1a305b35e8c
Ove Lindström <ove.lindstrom@assaabloy.com>
2016-09-14 10:29:52 +0200

    Explicit imports.

--- simpleclient_dropwizard/src/test/java/io/prometheus/client/dropwizard/DropwizardExportsTest.java
+++ simpleclient_dropwizard/src/test/java/io/prometheus/client/dropwizard/DropwizardExportsTest.java
@@ -9,7 +9,10 @@ import org.junit.Test;
 import java.io.IOException;
 import java.util.Arrays;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+
 
 public class DropwizardExportsTest {
 
