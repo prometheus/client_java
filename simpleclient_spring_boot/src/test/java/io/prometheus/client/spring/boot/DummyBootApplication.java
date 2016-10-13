97f24acf9505e214ceb03e0a569e635776ce8f2c
Eliezio Oliveira <github@eliez.io>
2016-10-13 00:15:56 -0300

    added /prometheus as a Spring Boot Endpoint

---
+++ simpleclient_spring_boot/src/test/java/io/prometheus/client/spring/boot/DummyBootApplication.java
@@ -0,0 +1,12 @@
+package io.prometheus.client.spring.boot;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Dummy class to satisfy Spring Boot Test requirement of a class annotated either with {code @SpringBootApplication} or
+ * {code @SpringBootConfiguration}.
+ */
+@SpringBootApplication
+class DummyBootApplication {
+
+}
