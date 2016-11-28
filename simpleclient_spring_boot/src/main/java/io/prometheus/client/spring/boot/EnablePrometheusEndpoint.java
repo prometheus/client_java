15000598be37e745993df55145ae8834b537b57c
Andrew Stuart <andrew.stuart2@gmail.com>
2016-11-28 13:06:47 -0700

    Remove MethodTimer from EnablePrometheusEndpoint

--- simpleclient_spring_boot/src/main/java/io/prometheus/client/spring/boot/EnablePrometheusEndpoint.java
+++ simpleclient_spring_boot/src/main/java/io/prometheus/client/spring/boot/EnablePrometheusEndpoint.java
@@ -38,7 +38,7 @@ import java.lang.annotation.Target;
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-@Import({PrometheusEndpointConfiguration.class, MethodTimer.class})
+@Import(PrometheusEndpointConfiguration.class)
 public @interface EnablePrometheusEndpoint {
 
 }
