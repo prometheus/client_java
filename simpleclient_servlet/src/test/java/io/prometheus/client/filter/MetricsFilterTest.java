b5b9cca8c3580774144e7dd09711e53e8445fcb3
Andrew Stuart <andrew.stuart2@gmail.com>
2016-12-05 23:58:27 -0700

    Add metrics filter test for buckets config

--- simpleclient_servlet/src/test/java/io/prometheus/client/filter/MetricsFilterTest.java
+++ simpleclient_servlet/src/test/java/io/prometheus/client/filter/MetricsFilterTest.java
@@ -2,27 +2,34 @@ package io.prometheus.client.filter;
 
 import io.prometheus.client.CollectorRegistry;
 import org.eclipse.jetty.http.HttpMethods;
+import org.junit.After;
 import org.junit.Test;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyObject;
 import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-/**
- * Note: When testing, you will want to make sure that you use a unique metric name (init the filter with a unique value
- * returned for {@link MetricsFilter.METRIC_NAME_PARAM}) for each test. Otherwise, you will see spurious error messages.
- */
-
 public class MetricsFilterTest {
     MetricsFilter f = new MetricsFilter();
+
+    @After
+    public void clear() {
+        CollectorRegistry.defaultRegistry.clear();
+    }
 
     @Test
     public void init() throws Exception {
@@ -52,7 +59,7 @@ public class MetricsFilterTest {
 
         final Double sampleValue = CollectorRegistry.defaultRegistry.getSampleValue(metricName + "_count", new String[]{"path", "method"}, new String[]{"/foo/bar/baz/bang", HttpMethods.GET});
         assertNotNull(sampleValue);
-        assertEquals(sampleValue, 1, 0.0001);
+        assertEquals(1, sampleValue, 0.0001);
     }
 
     @Test
@@ -72,7 +79,41 @@ public class MetricsFilterTest {
 
         final Double sampleValue = CollectorRegistry.defaultRegistry.getSampleValue(MetricsFilter.DEFAULT_FILTER_NAME + "_count", new String[]{"path", "method"}, new String[]{"/foo/bar/baz", HttpMethods.GET});
         assertNotNull(sampleValue);
-        assertEquals(sampleValue, 1, 0.0001);
+        assertEquals(1, sampleValue, 0.0001);
+    }
+
+    @Test
+    public void testBucketsAndName() throws Exception {
+        HttpServletRequest req = mock(HttpServletRequest.class);
+        when(req.getRequestURI()).thenReturn("/foo/bar/baz/bang");
+        when(req.getMethod()).thenReturn(HttpMethods.POST);
+
+        FilterChain c = mock(FilterChain.class);
+        doAnswer(new Answer<Void>() {
+            @Override
+            public Void answer(InvocationOnMock invocationOnMock) throws Throwable {
+                Thread.sleep(100);
+                return null;
+            }
+        }).when(c).doFilter(any(HttpServletRequest.class), any(HttpServletResponse.class));
+
+        FilterConfig cfg = mock(FilterConfig.class);
+        when(cfg.getInitParameter(MetricsFilter.BUCKET_CONFIG_PARAM)).thenReturn("0.01,0.05,0.1,0.15,0.25");
+        when(cfg.getInitParameter(MetricsFilter.METRIC_NAME_PARAM)).thenReturn("foo");
+
+        HttpServletResponse res = mock(HttpServletResponse.class);
+
+        f.init(cfg);
+
+        f.doFilter(req, res, c);
+
+        final Double sum = CollectorRegistry.defaultRegistry.getSampleValue("foo_sum", new String[]{"path", "method"}, new String[]{"/foo/bar/baz", HttpMethods.POST});
+        assertEquals(0.1, sum, 0.001);
+
+        final Double le05 = CollectorRegistry.defaultRegistry.getSampleValue("foo_bucket", new String[]{"path", "method", "le"}, new String[]{"/foo/bar/baz", HttpMethods.POST, "0.05"});
+        assertEquals(0, le05, 0.0001);
+        final Double le15 = CollectorRegistry.defaultRegistry.getSampleValue("foo_bucket", new String[]{"path", "method", "le"}, new String[]{"/foo/bar/baz", HttpMethods.POST, "0.15"});
+        assertEquals(1, le15, 0.0001);
     }
 
 }
