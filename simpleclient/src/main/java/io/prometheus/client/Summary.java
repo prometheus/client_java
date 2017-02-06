2d62bacda2a3dc7acb993afd213fd52a2cca9428
Andreas Pillath <ap@ise-online.com>
2017-02-06 08:02:56 +0100

    #193 omit import Child.Value, and qualify Child.Value

--- simpleclient/src/main/java/io/prometheus/client/Summary.java
+++ simpleclient/src/main/java/io/prometheus/client/Summary.java
@@ -1,7 +1,6 @@
 package io.prometheus.client;
 
 import io.prometheus.client.CKMSQuantiles.Quantile;
-import io.prometheus.client.Summary.Child.Value;
 
 import java.io.Closeable;
 import java.io.IOException;
@@ -314,7 +313,7 @@ public class Summary extends SimpleCollector<Summary.Child> implements Counter.D
    * <p>
    * <em>Warning:</em> The definition of {@link Value} is subject to change.
    */
-  public Value get() {
+  public Child.Value get() {
     return noLabelsChild.get();
   }
 
