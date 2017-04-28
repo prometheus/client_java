fa3ce4c8aa6424ac3883830b7d27faa6a59fc7a1
Henrique Miranda <henriquesantanademiranda@royalfloraholland.com>
2017-04-28 11:54:13 +0200

    Refactored MVCEndpoint to mirror spring boot actuator class hierarchy

--- simpleclient_spring_boot/src/main/java/io/prometheus/client/spring/boot/PrometheusMVCEndpoint.java
+++ simpleclient_spring_boot/src/main/java/io/prometheus/client/spring/boot/PrometheusMVCEndpoint.java
@@ -1,6 +1,7 @@
 package io.prometheus.client.spring.boot;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.endpoint.mvc.AbstractEndpointMvcAdapter;
 import org.springframework.boot.actuate.endpoint.mvc.EndpointMvcAdapter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -8,24 +9,20 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-public class PrometheusMVCEndpoint extends EndpointMvcAdapter {
-
-	private final PrometheusEndpoint delegate;
+public class PrometheusMVCEndpoint extends AbstractEndpointMvcAdapter<PrometheusEndpoint> {
 
 	@Autowired
 	public PrometheusMVCEndpoint(PrometheusEndpoint delegate) {
 		super(delegate);
-		this.delegate = delegate;
 	}
 
 	@RequestMapping(value = "", method = RequestMethod.GET, produces = MediaType.TEXT_PLAIN_VALUE)
-	public ResponseEntity<String> invoke() {
-		if (delegate.isEnabled()) {
-			return delegate.invoke();
+	public Object invoke() {
+		if (!getDelegate().isEnabled()) {
+			// Shouldn't happen because the request mapping should not be registered
+			return getDisabledResponse();
 		} else {
-			return new ResponseEntity<String>(HttpStatus.NOT_FOUND);
+			return getDelegate().invoke();
 		}
 	}
-
-
 }
