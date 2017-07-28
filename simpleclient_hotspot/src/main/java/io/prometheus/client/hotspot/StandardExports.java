ce76a494acd4f4077f01b6cea40e4329b0d54fac
Alin Sinpalean <alin.sinpalean@amanaadvisors.com>
2017-07-28 09:25:39 +0200

    Make StandardExports work with modules and/or IBM JVMs (#281)
    Make StandardExports work in restricted environments or IBM JVMs where com.sun.management.UnixOperatingSystemMXBean is not available. Use reflection instead to collect file descriptor metrics.
    
    Use reflection to recursively try invoking a method on the concrete class then on all the interfaces it implements until either success or all attempts have failed

--- simpleclient_hotspot/src/main/java/io/prometheus/client/hotspot/StandardExports.java
+++ simpleclient_hotspot/src/main/java/io/prometheus/client/hotspot/StandardExports.java
@@ -1,6 +1,5 @@
 package io.prometheus.client.hotspot;
 
-import com.sun.management.UnixOperatingSystemMXBean;
 import io.prometheus.client.Collector;
 import io.prometheus.client.CounterMetricFamily;
 import io.prometheus.client.GaugeMetricFamily;
@@ -12,6 +11,7 @@ import java.io.IOException;
 import java.lang.management.ManagementFactory;
 import java.lang.management.OperatingSystemMXBean;
 import java.lang.management.RuntimeMXBean;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
@@ -36,7 +36,6 @@ public class StandardExports extends Collector {
   private final StatusReader statusReader;
   private final OperatingSystemMXBean osBean;
   private final RuntimeMXBean runtimeBean;
-  private final boolean unix;
   private final boolean linux;
 
   public StandardExports() {
@@ -49,43 +48,42 @@ public class StandardExports extends Collector {
       this.statusReader = statusReader;
       this.osBean = osBean;
       this.runtimeBean = runtimeBean;
-      this.unix = (osBean instanceof UnixOperatingSystemMXBean);
       this.linux = (osBean.getName().indexOf("Linux") == 0);
   }
 
   private final static double KB = 1024;
 
+  @Override
   public List<MetricFamilySamples> collect() {
     List<MetricFamilySamples> mfs = new ArrayList<MetricFamilySamples>();
 
     try {
-      /*
-      There are two interfaces com.ibm.lang.management.OperatingSystemMXBean and com.sun.management.OperatingSystemMXBean,
-      but no common interface which exposes getProcessCpuTime. Hence call on the implementing class, which is default access,
-      so use reflection hack to set it accessible.
-      */
-      Method method = osBean.getClass().getMethod("getProcessCpuTime", null);
-      if (!method.isAccessible()) {
-        method.setAccessible(true);
-      }
-      Long l = (Long) method.invoke(osBean);
+      // There exist at least 2 similar but unrelated UnixOperatingSystemMXBean interfaces, in
+      // com.sun.management and com.ibm.lang.management. Hence use reflection and recursively go
+      // through implemented interfaces until the method can be made accessible and invoked.
+      Long processCpuTime = callLongGetter("getProcessCpuTime", osBean);
       mfs.add(new CounterMetricFamily("process_cpu_seconds_total", "Total user and system CPU time spent in seconds.",
-          l / NANOSECONDS_PER_SECOND));
+          processCpuTime / NANOSECONDS_PER_SECOND));
     }
     catch (Exception e) {
       LOGGER.log(Level.FINE,"Could not access process cpu time", e);
     }
 
-
     mfs.add(new GaugeMetricFamily("process_start_time_seconds", "Start time of the process since unix epoch in seconds.",
         runtimeBean.getStartTime() / MILLISECONDS_PER_SECOND));
 
-    if (unix) {
-      UnixOperatingSystemMXBean unixBean = (UnixOperatingSystemMXBean) osBean;
-      mfs.add(new GaugeMetricFamily("process_open_fds", "Number of open file descriptors.",
-          unixBean.getOpenFileDescriptorCount()));
-      mfs.add(new GaugeMetricFamily("process_max_fds", "Maximum number of open file descriptors.",
-          unixBean.getMaxFileDescriptorCount()));
+    // There exist at least 2 similar but unrelated UnixOperatingSystemMXBean interfaces, in
+    // com.sun.management and com.ibm.lang.management. Hence use reflection and recursively go
+    // through implemented interfaces until the method can be made accessible and invoked.
+    try {
+      Long openFdCount = callLongGetter("getOpenFileDescriptorCount", osBean);
+      mfs.add(new GaugeMetricFamily(
+          "process_open_fds", "Number of open file descriptors.", openFdCount));
+      Long maxFdCount = callLongGetter("getMaxFileDescriptorCount", osBean);
+      mfs.add(new GaugeMetricFamily(
+          "process_max_fds", "Maximum number of open file descriptors.", maxFdCount));
+    } catch (Exception e) {
+      // Ignore, expected on non-Unix OSs.
     }
 
     // There's no standard Java or POSIX way to get memory stats,
@@ -99,6 +97,49 @@ public class StandardExports extends Collector {
       }
     }
     return mfs;
+  }
+
+  static Long callLongGetter(String getterName, Object obj)
+      throws NoSuchMethodException, InvocationTargetException {
+    return callLongGetter(obj.getClass().getMethod(getterName), obj);
+  }
+
+  /**
+   * Attempts to call a method either directly or via one of the implemented interfaces.
+   * <p>
+   * A Method object refers to a specific method declared in a specific class. The first invocation
+   * might happen with method == SomeConcreteClass.publicLongGetter() and will fail if
+   * SomeConcreteClass is not public. We then recurse over all interfaces implemented by
+   * SomeConcreteClass (or extended by those interfaces and so on) until we eventually invoke
+   * callMethod() with method == SomePublicInterface.publicLongGetter(), which will then succeed.
+   * <p>
+   * There is a built-in assumption that the method will never return null (or, equivalently, that
+   * it returns the primitive data type, i.e. {@code long} rather than {@code Long}). If this
+   * assumption doesn't hold, the method might be called repeatedly and the returned value will be
+   * the one produced by the last call.
+   */
+  static Long callLongGetter(Method method, Object obj) throws InvocationTargetException  {
+    try {
+      return (Long) method.invoke(obj);
+    } catch (IllegalAccessException e) {
+      // Expected, the declaring class or interface might not be public.
+    }
+
+    // Iterate over all implemented/extended interfaces and attempt invoking the method with the
+    // same name and parameters on each.
+    for (Class<?> clazz : method.getDeclaringClass().getInterfaces()) {
+      try {
+        Method interfaceMethod = clazz.getMethod(method.getName(), method.getParameterTypes());
+        Long result = callLongGetter(interfaceMethod, obj);
+        if (result != null) {
+          return result;
+        }
+      } catch (NoSuchMethodException e) {
+        // Expected, class might implement multiple, unrelated interfaces.
+      }
+    }
+
+    return null;
   }
 
   void collectMemoryMetricsLinux(List<MetricFamilySamples> mfs) {
