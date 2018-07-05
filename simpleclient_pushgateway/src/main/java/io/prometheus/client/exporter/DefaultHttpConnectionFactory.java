ba03f8cd5a0b668e03d95cf927cafd59eafc524a
bitsal <beletsky.andrew@gmail.com>
2018-07-06 00:26:09 +0300

    Use special connection factory instead of inheritance.
    (cherry picked from commit 3f3f88d)
    Signed-off-by: bitsal <beletsky.andrew@gmail.com>

---
+++ simpleclient_pushgateway/src/main/java/io/prometheus/client/exporter/DefaultHttpConnectionFactory.java
@@ -0,0 +1,12 @@
+package io.prometheus.client.exporter;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class DefaultHttpConnectionFactory implements HttpConnectionFactory {
+    @Override
+    public HttpURLConnection create(String url) throws IOException {
+        return (HttpURLConnection) new URL(url).openConnection();
+    }
+}
