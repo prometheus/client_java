a5933f95ffadbecb9f29e1adbb3b4d958abe8f8f
Raphael P. Barazzutti <raphael@barazzutti.net>
2018-08-16 18:30:49 +0200

    fix XML code in javadoc
    Signed-off-by: Raphael P. Barazzutti <raphael@barazzutti.net>

--- simpleclient_servlet/src/main/java/io/prometheus/client/filter/MetricsFilter.java
+++ simpleclient_servlet/src/main/java/io/prometheus/client/filter/MetricsFilter.java
@@ -15,19 +15,18 @@ import java.io.IOException;
  * The MetricsFilter class exists to provide a high-level filter that enables tunable collection of metrics for Servlet
  * performance.
  *
- * The Histogram name itself is required, and configured with a {@code metric-name} init parameter.
+ * <p>The Histogram name itself is required, and configured with a {@code metric-name} init parameter.
  *
- * The help parameter, configured with the {@code help} init parameter, is not required but strongly recommended.
+ * <p>The help parameter, configured with the {@code help} init parameter, is not required but strongly recommended.
  *
- * By default, this filter will provide metrics that distinguish only 1 level deep for the request path
+ * <p>By default, this filter will provide metrics that distinguish only 1 level deep for the request path
  * (including servlet context path), but can be configured with the {@code path-components} init parameter. Any number
  * provided that is less than 1 will provide the full path granularity (warning, this may affect performance).
  *
- * The Histogram buckets can be configured with a {@code buckets} init parameter whose value is a comma-separated list
+ * <p>The Histogram buckets can be configured with a {@code buckets} init parameter whose value is a comma-separated list
  * of valid {@code double} values.
  *
- * <pre>
- * {@code
+ * <pre>{@code
  * <filter>
  *   <filter-name>prometheusFilter</filter-name>
  *   <filter-class>io.prometheus.client.filter.MetricsFilter</filter-class>
@@ -48,8 +47,7 @@ import java.io.IOException;
  *      <param-value>0</param-value>
  *   </init-param>
  * </filter>
- * }
- * </pre>
+ * }</pre>
  *
  * @author Andrew Stuart &lt;andrew.stuart2@gmail.com&gt;
  */
