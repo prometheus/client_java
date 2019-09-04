10fa80b9fc9a3be66d95fbad9760e337ed465495
ugniusb <ugniusb@wix.com>
2019-09-04 20:46:03 +0300

    Don't sanitize away colons in metric names (#498)
    The docs specify that colons (`:`) are allowed in metric names, but
    `Collector.sanitizeMetricName` removes them.
    
    Signed-off-by: ugniusb <ugniusb@wix.com>

--- simpleclient/src/test/java/io/prometheus/client/CollectorTest.java
+++ simpleclient/src/test/java/io/prometheus/client/CollectorTest.java
@@ -9,5 +9,6 @@ public class CollectorTest {
   public void sanitizeMetricName() throws Exception {
       assertEquals("_hoge", Collector.sanitizeMetricName("0hoge"));
       assertEquals("foo_bar0", Collector.sanitizeMetricName("foo.bar0"));
+      assertEquals(":baz::", Collector.sanitizeMetricName(":baz::"));
   }
 }
