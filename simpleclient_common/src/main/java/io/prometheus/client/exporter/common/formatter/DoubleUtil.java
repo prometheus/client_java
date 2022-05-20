5bb88c98145f1003677cd59d483266887771685b
daojun <jmtao@streamnative.io>
2022-05-21 02:32:10 +0800

    fix DoubleUtil

--- simpleclient_common/src/main/java/io/prometheus/client/exporter/common/formatter/DoubleUtil.java
+++ simpleclient_common/src/main/java/io/prometheus/client/exporter/common/formatter/DoubleUtil.java
@@ -7,26 +7,25 @@ import java.io.Writer;
 import java.lang.reflect.Field;
 
 class DoubleUtil {
-  private static final Unsafe UNSAFE;
-  private static final long COUNT_OFFSET;
-  private static final long VALUE_OFFSET;
-  private static final ThreadLocal<StringBuilder> STRING_BUILDER_CACHE =
-      new ThreadLocal<StringBuilder>();
+  private static Unsafe unsafe;
+  private static long countOffset;
+  private static long valueOffset;
+  private static ThreadLocal<StringBuilder> stringBuilderCache;
+  private static boolean initialized = true;
 
   static {
-    Class<Unsafe> clazz = Unsafe.class;
     try {
-      Field field = clazz.getDeclaredField("theUnsafe");
+      Class<Unsafe> unsafeKlass = Unsafe.class;
+      Field field = unsafeKlass.getDeclaredField("theUnsafe");
       field.setAccessible(true);
-      UNSAFE = (Unsafe) field.get(null);
+      unsafe = (Unsafe) field.get(null);
 
-      Class<?> superClass = StringBuilder.class.getSuperclass();
-      COUNT_OFFSET = UNSAFE.objectFieldOffset(superClass.getDeclaredField("count"));
-      VALUE_OFFSET = UNSAFE.objectFieldOffset(superClass.getDeclaredField("value"));
-    } catch (NoSuchFieldException e) {
-      throw new Error();
-    } catch (IllegalAccessException ex) {
-      throw new Error();
+      Class<?> superKlass = StringBuilder.class.getSuperclass();
+      countOffset = unsafe.objectFieldOffset(superKlass.getDeclaredField("count"));
+      valueOffset = unsafe.objectFieldOffset(superKlass.getDeclaredField("value"));
+      stringBuilderCache = new ThreadLocal<StringBuilder>();
+    } catch (Exception e) {
+      initialized = false;
     }
   }
 
@@ -38,21 +37,25 @@ class DoubleUtil {
    * @throws IOException
    */
   static void append(Writer writer, double v) throws IOException {
-    StringBuilder builder = STRING_BUILDER_CACHE.get();
-    if (builder == null) {
-      builder = new StringBuilder();
-      STRING_BUILDER_CACHE.set(builder);
+    if (initialized) {
+      StringBuilder builder = stringBuilderCache.get();
+      if (builder == null) {
+        builder = new StringBuilder();
+        stringBuilderCache.set(builder);
+      }
+
+      builder.append(v);
+
+      byte[] value = (byte[]) unsafe.getObject(builder, valueOffset);
+      int count = unsafe.getInt(builder, countOffset);
+
+      for (int a = 0; a < count; a++) {
+        writer.write(value[a]);
+      }
+
+      unsafe.getAndSetInt(builder, countOffset, 0);
+    } else {
+      writer.write(Double.toString(v));
     }
-
-    builder.append(v);
-
-    byte[] value = (byte[]) UNSAFE.getObject(builder, VALUE_OFFSET);
-    int count = UNSAFE.getInt(builder, COUNT_OFFSET);
-
-    for (int a = 0; a < count; a++) {
-      writer.write(value[a]);
-    }
-
-    UNSAFE.getAndSetInt(builder, COUNT_OFFSET, 0);
   }
 }
