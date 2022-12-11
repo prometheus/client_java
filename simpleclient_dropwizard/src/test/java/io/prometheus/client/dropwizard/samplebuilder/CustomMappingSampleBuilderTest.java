47254cf2c4d18e6184629eb6907aac5991476500
tt0suzy <tt0suzy@gmail.com>
2022-12-11 12:16:35 -0600

    change single labels back to HashMap

--- simpleclient_dropwizard/src/test/java/io/prometheus/client/dropwizard/samplebuilder/CustomMappingSampleBuilderTest.java
+++ simpleclient_dropwizard/src/test/java/io/prometheus/client/dropwizard/samplebuilder/CustomMappingSampleBuilderTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -33,7 +34,7 @@ public class CustomMappingSampleBuilderTest {
 
     @Test
     public void test_WHEN_OneMatch_THEN_ShouldReturnConverted() {
-        final Map<String, String> labels = new LinkedHashMap<String, String>();
+        final Map<String, String> labels = new HashMap<String, String>();
         labels.put("service", "${0}");
         final MapperConfig mapperConfig = new MapperConfig(
                 "app.okhttpclient.client.HttpClient.*.total",
@@ -57,7 +58,7 @@ public class CustomMappingSampleBuilderTest {
 
     @Test
     public void test_WHEN_MoreMatches_THEN_ShouldReturnFirstOne() {
-        final Map<String, String> labels = new LinkedHashMap<String, String>();
+        final Map<String, String> labels = new HashMap<String, String>();
         labels.put("service", "${0}");
         final MapperConfig mapperConfig = new MapperConfig(
                 "app.okhttpclient.client.HttpClient.*.total",
