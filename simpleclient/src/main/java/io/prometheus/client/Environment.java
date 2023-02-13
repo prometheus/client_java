68560b66c7b5462b4becdcb597d839db391ad422
Doug Hoard <doug.hoard@gmail.com>
2023-02-12 22:35:19 -0500

    Added code to use system property 'prometheus.disable.created.series'

--- simpleclient/src/main/java/io/prometheus/client/Environment.java
+++ simpleclient/src/main/java/io/prometheus/client/Environment.java
@@ -6,9 +6,18 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 public class Environment {
 
-    private static final String DISABLE_CREATED_SERIES = "PROMETHEUS_DISABLE_CREATED_SERIES";
+    private static final String DISABLE_CREATED_SERIES_ENVIRONMENT_VARIABLE = "PROMETHEUS_DISABLE_CREATED_SERIES";
+    private static final String DISABLE_CREATED_SERIES_SYSTEM_PROPERTY = "prometheus.disable.created.series";
+
     private static final List<String> DISABLE_CREATED_SERIES_TRUE = Arrays.asList("true", "1", "t");
-    private static AtomicBoolean includeCreatedSeries = new AtomicBoolean(!isTrue(DISABLE_CREATED_SERIES));
+
+    private Environment() {
+        // DO NOTHING
+    }
+
+    private static AtomicBoolean includeCreatedSeries =
+            new AtomicBoolean(
+                    !isTrue(DISABLE_CREATED_SERIES_SYSTEM_PROPERTY, DISABLE_CREATED_SERIES_ENVIRONMENT_VARIABLE));
 
     public static boolean includeCreatedSeries() {
         return includeCreatedSeries.get();
@@ -22,11 +31,17 @@ public class Environment {
         includeCreatedSeries.set(false);
     }
 
-    private static boolean isTrue(String envVarName) {
-        String stringValue = System.getenv(envVarName);
+    private static boolean isTrue(String systemPropertyName, String environmentVariableName) {
+        String stringValue = System.getProperty(systemPropertyName);
         if (stringValue != null) {
             return DISABLE_CREATED_SERIES_TRUE.contains(stringValue.toLowerCase());
         }
+
+        stringValue = System.getenv(environmentVariableName);
+        if (stringValue != null) {
+            return DISABLE_CREATED_SERIES_TRUE.contains(stringValue.toLowerCase());
+        }
+
         return false;
     }
 }
