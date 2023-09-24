3eca096f0b2930887221ef774b7439da4bac1547
Fabian Stäber <fabian@fstab.de>
2023-09-24 22:37:03 +0200

    Move simpleclient to prometheus-metrics modules
    Signed-off-by: Fabian Stäber <fabian@fstab.de>

--- simpleclient/src/main/java/io/prometheus/client/Supplier.java
+++
@@ -1,8 +0,0 @@
-package io.prometheus.client;
-
-/**
- * Replacement for Java 8's {@code java.util.function.Supplier} for compatibility with Java versions &lt; 8.
- */
-public interface Supplier<T> {
-    T get();
-}
