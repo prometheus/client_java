ac0a930dd213bc598030af417e58478ba29d669e
Doug Hoard <dhoard@users.noreply.github.com>
2024-07-30 12:34:29 -0400

    Added methods (#978)
    Signed-off-by: dhoard <doug.hoard@gmail.com>

--- prometheus-metrics-model/src/main/java/io/prometheus/metrics/model/snapshots/Label.java
+++ prometheus-metrics-model/src/main/java/io/prometheus/metrics/model/snapshots/Label.java
@@ -1,5 +1,7 @@
 package io.prometheus.metrics.model.snapshots;
 
+import java.util.Objects;
+
 /**
  * Utility for iterating over {@link Labels}.
  */
@@ -16,6 +18,7 @@ public final class Label implements Comparable<Label> {
     public String getName() {
         return name;
     }
+
     public String getValue() {
         return value;
     }
@@ -24,5 +27,26 @@ public final class Label implements Comparable<Label> {
     public int compareTo(Label other) {
         int nameCompare = name.compareTo(other.name);
         return nameCompare != 0 ? nameCompare : value.compareTo(other.value);
+    }
+
+    @Override
+    public String toString() {
+        return "Label{" +
+                "name='" + name + '\'' +
+                ", value='" + value + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Label label = (Label) o;
+        return Objects.equals(name, label.name) && Objects.equals(value, label.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, value);
     }
 }
