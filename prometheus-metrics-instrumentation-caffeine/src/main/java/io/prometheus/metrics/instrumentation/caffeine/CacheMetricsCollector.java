7e5f615c136f67327bbb6da6a11030b3ba1ff8e5
Petar Heyken <p.heyken@rebuy.com>
2024-08-30 12:45:14 +0200

    add support for caffeine instrumentation with 1.X.X
    Signed-off-by: Petar Heyken <p.heyken@rebuy.com>

--- prometheus-metrics-instrumentation-caffeine/src/main/java/io/prometheus/metrics/instrumentation/caffeine/CacheMetricsCollector.java
+++ prometheus-metrics-instrumentation-caffeine/src/main/java/io/prometheus/metrics/instrumentation/caffeine/CacheMetricsCollector.java
@@ -1,15 +1,16 @@
-package io.prometheus.client.cache.caffeine;
+package io.prometheus.metrics.instrumentation.caffeine;
 
 import com.github.benmanes.caffeine.cache.AsyncCache;
 import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.LoadingCache;
 import com.github.benmanes.caffeine.cache.stats.CacheStats;
-import io.prometheus.client.Collector;
-import io.prometheus.client.CounterMetricFamily;
-import io.prometheus.client.GaugeMetricFamily;
-import io.prometheus.client.SummaryMetricFamily;
+import io.prometheus.metrics.model.registry.MultiCollector;
+import io.prometheus.metrics.model.snapshots.CounterSnapshot;
+import io.prometheus.metrics.model.snapshots.GaugeSnapshot;
+import io.prometheus.metrics.model.snapshots.Labels;
+import io.prometheus.metrics.model.snapshots.MetricSnapshots;
+import io.prometheus.metrics.model.snapshots.SummarySnapshot;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -40,7 +41,7 @@ import java.util.concurrent.ConcurrentMap;
  *     caffeine_cache_estimated_size{cache="mycache"} 5.0
  * </pre>
  *
- * Additionally if the cache includes a loader, the following metrics would be provided:
+ * Additionally, if the cache includes a loader, the following metrics would be provided:
  * <pre>
  *     caffeine_cache_load_failure_total{cache="mycache"} 2.0
  *     caffeine_cache_loads_total{cache="mycache"} 7.0
@@ -49,7 +50,9 @@ import java.util.concurrent.ConcurrentMap;
  * </pre>
  *
  */
-public class CacheMetricsCollector extends Collector {
+public class CacheMetricsCollector implements MultiCollector {
+    private static final double NANOSECONDS_PER_SECOND = 1_000_000_000.0;
+
     protected final ConcurrentMap<String, Cache> children = new ConcurrentHashMap<String, Cache>();
 
     /**
@@ -97,69 +100,133 @@ public class CacheMetricsCollector extends Collector {
     }
 
     @Override
-    public List<MetricFamilySamples> collect() {
-        List<MetricFamilySamples> mfs = new ArrayList<MetricFamilySamples>();
-        List<String> labelNames = Arrays.asList("cache");
+    public MetricSnapshots collect() {
+        final MetricSnapshots.Builder metricSnapshotsBuilder = MetricSnapshots.builder();
+        final List<String> labelNames = Arrays.asList("cache");
 
-        CounterMetricFamily cacheHitTotal = new CounterMetricFamily("caffeine_cache_hit_total",
-                "Cache hit totals", labelNames);
-        mfs.add(cacheHitTotal);
+        final CounterSnapshot.Builder cacheHitTotal = CounterSnapshot.builder()
+            .name("caffeine_cache_hit")
+            .help("Cache hit totals");
 
-        CounterMetricFamily cacheMissTotal = new CounterMetricFamily("caffeine_cache_miss_total",
-                "Cache miss totals", labelNames);
-        mfs.add(cacheMissTotal);
+        final CounterSnapshot.Builder cacheMissTotal = CounterSnapshot.builder()
+            .name("caffeine_cache_miss")
+            .help("Cache miss totals");
 
-        CounterMetricFamily cacheRequestsTotal = new CounterMetricFamily("caffeine_cache_requests_total",
-                "Cache request totals, hits + misses", labelNames);
-        mfs.add(cacheRequestsTotal);
+        final CounterSnapshot.Builder cacheRequestsTotal = CounterSnapshot.builder()
+            .name("caffeine_cache_requests")
+            .help("Cache request totals, hits + misses");
 
-        CounterMetricFamily cacheEvictionTotal = new CounterMetricFamily("caffeine_cache_eviction_total",
-                "Cache eviction totals, doesn't include manually removed entries", labelNames);
-        mfs.add(cacheEvictionTotal);
+        final CounterSnapshot.Builder cacheEvictionTotal = CounterSnapshot.builder()
+            .name("caffeine_cache_eviction")
+            .help("Cache eviction totals, doesn't include manually removed entries");
 
-        GaugeMetricFamily cacheEvictionWeight = new GaugeMetricFamily("caffeine_cache_eviction_weight",
-                "Cache eviction weight", labelNames);
-        mfs.add(cacheEvictionWeight);
+        final GaugeSnapshot.Builder cacheEvictionWeight = GaugeSnapshot.builder()
+            .name("caffeine_cache_eviction_weight")
+            .help("Cache eviction weight");
 
-        CounterMetricFamily cacheLoadFailure = new CounterMetricFamily("caffeine_cache_load_failure_total",
-                "Cache load failures", labelNames);
-        mfs.add(cacheLoadFailure);
+        final CounterSnapshot.Builder cacheLoadFailure = CounterSnapshot.builder()
+            .name("caffeine_cache_load_failure")
+            .help("Cache load failures");
 
-        CounterMetricFamily cacheLoadTotal = new CounterMetricFamily("caffeine_cache_loads_total",
-                "Cache loads: both success and failures", labelNames);
-        mfs.add(cacheLoadTotal);
+        final CounterSnapshot.Builder cacheLoadTotal = CounterSnapshot.builder()
+            .name("caffeine_cache_loads")
+            .help("Cache loads: both success and failures");
 
-        GaugeMetricFamily cacheSize = new GaugeMetricFamily("caffeine_cache_estimated_size",
-                "Estimated cache size", labelNames);
-        mfs.add(cacheSize);
+        final GaugeSnapshot.Builder cacheSize = GaugeSnapshot.builder()
+            .name("caffeine_cache_estimated_size")
+            .help("Estimated cache size");
 
-        SummaryMetricFamily cacheLoadSummary = new SummaryMetricFamily("caffeine_cache_load_duration_seconds",
-                "Cache load duration: both success and failures", labelNames);
-        mfs.add(cacheLoadSummary);
+        final SummarySnapshot.Builder cacheLoadSummary = SummarySnapshot.builder()
+            .name("caffeine_cache_load_duration_seconds")
+            .help("Cache load duration: both success and failures");
 
-        for(Map.Entry<String, Cache> c: children.entrySet()) {
-            List<String> cacheName = Arrays.asList(c.getKey());
-            CacheStats stats = c.getValue().stats();
+        for (final Map.Entry<String, Cache> c: children.entrySet()) {
+            final List<String> cacheName = Arrays.asList(c.getKey());
+            final Labels labels = Labels.of(labelNames, cacheName);
 
-            try{
-                cacheEvictionWeight.addMetric(cacheName, stats.evictionWeight());
+            final CacheStats stats = c.getValue().stats();
+
+            try {
+                cacheEvictionWeight.dataPoint(
+                    GaugeSnapshot.GaugeDataPointSnapshot.builder()
+                        .labels(labels)
+                        .value(stats.evictionWeight())
+                        .build()
+                );
             } catch (Exception e) {
                 // EvictionWeight metric is unavailable, newer version of Caffeine is needed.
             }
 
-            cacheHitTotal.addMetric(cacheName, stats.hitCount());
-            cacheMissTotal.addMetric(cacheName, stats.missCount());
-            cacheRequestsTotal.addMetric(cacheName, stats.requestCount());
-            cacheEvictionTotal.addMetric(cacheName, stats.evictionCount());
-            cacheSize.addMetric(cacheName, c.getValue().estimatedSize());
+            cacheHitTotal.dataPoint(
+                CounterSnapshot.CounterDataPointSnapshot.builder()
+                    .labels(labels)
+                    .value(stats.hitCount())
+                    .build()
+            );
 
-            if(c.getValue() instanceof LoadingCache) {
-                cacheLoadFailure.addMetric(cacheName, stats.loadFailureCount());
-                cacheLoadTotal.addMetric(cacheName, stats.loadCount());
+            cacheMissTotal.dataPoint(
+                CounterSnapshot.CounterDataPointSnapshot.builder()
+                    .labels(labels)
+                    .value(stats.missCount())
+                    .build()
+            );
 
-                cacheLoadSummary.addMetric(cacheName, stats.loadCount(), stats.totalLoadTime() / Collector.NANOSECONDS_PER_SECOND);
+            cacheRequestsTotal.dataPoint(
+                CounterSnapshot.CounterDataPointSnapshot.builder()
+                    .labels(labels)
+                    .value(stats.requestCount())
+                    .build()
+            );
+
+            cacheEvictionTotal.dataPoint(
+                CounterSnapshot.CounterDataPointSnapshot.builder()
+                    .labels(labels)
+                    .value(stats.evictionCount())
+                    .build()
+            );
+
+            cacheSize.dataPoint(
+                GaugeSnapshot.GaugeDataPointSnapshot.builder()
+                    .labels(labels)
+                    .value(c.getValue().estimatedSize())
+                    .build()
+            );
+
+            if (c.getValue() instanceof LoadingCache) {
+                cacheLoadFailure.dataPoint(
+                    CounterSnapshot.CounterDataPointSnapshot.builder()
+                        .labels(labels)
+                        .value(stats.loadFailureCount())
+                        .build()
+                );
+
+                cacheLoadTotal.dataPoint(
+                    CounterSnapshot.CounterDataPointSnapshot.builder()
+                        .labels(labels)
+                        .value(stats.loadCount())
+                        .build()
+                );
+
+                cacheLoadSummary.dataPoint(
+                    SummarySnapshot.SummaryDataPointSnapshot.builder()
+                        .labels(labels)
+                        .count(stats.loadCount())
+                        .sum(stats.totalLoadTime() / NANOSECONDS_PER_SECOND)
+                        .build()
+                );
             }
         }
-        return mfs;
+
+        return metricSnapshotsBuilder
+            .metricSnapshot(cacheHitTotal.build())
+            .metricSnapshot(cacheMissTotal.build())
+            .metricSnapshot(cacheRequestsTotal.build())
+            .metricSnapshot(cacheEvictionTotal.build())
+            .metricSnapshot(cacheEvictionWeight.build())
+            .metricSnapshot(cacheLoadFailure.build())
+            .metricSnapshot(cacheLoadTotal.build())
+            .metricSnapshot(cacheSize.build())
+            .metricSnapshot(cacheLoadSummary.build())
+            .build();
     }
 }
