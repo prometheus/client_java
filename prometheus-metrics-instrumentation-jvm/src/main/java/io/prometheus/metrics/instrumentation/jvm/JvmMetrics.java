14c09083e75b807837d5b1da80688c01d0ff1b73
Mickael Maison <mimaison@users.noreply.github.com>
2024-09-30 16:37:07 +0200

    Make JvmMetrics.register idempotent with the default registry (#987)
    * Make JvmMetrics.register idempotent with the default registry
    
    Signed-off-by: Mickael Maison <mickael.maison@gmail.com>
    
    * Make JvmMetrics.register() methods idempotent
    
    Signed-off-by: Mickael Maison <mickael.maison@gmail.com>
    
    ---------
    
    Signed-off-by: Mickael Maison <mickael.maison@gmail.com>

--- prometheus-metrics-instrumentation-jvm/src/main/java/io/prometheus/metrics/instrumentation/jvm/JvmMetrics.java
+++ prometheus-metrics-instrumentation-jvm/src/main/java/io/prometheus/metrics/instrumentation/jvm/JvmMetrics.java
@@ -3,7 +3,8 @@ package io.prometheus.metrics.instrumentation.jvm;
 import io.prometheus.metrics.config.PrometheusProperties;
 import io.prometheus.metrics.model.registry.PrometheusRegistry;
 
-import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Registers all JVM metrics. Example usage:
@@ -13,15 +14,14 @@ import java.util.concurrent.atomic.AtomicBoolean;
  */
 public class JvmMetrics {
 
-    private static AtomicBoolean registeredWithTheDefaultRegistry = new AtomicBoolean(false);
-
+    private static final Set<PrometheusRegistry> REGISTERED = ConcurrentHashMap.newKeySet();
     public static Builder builder() {
         return new Builder(PrometheusProperties.get());
     }
 
     // Note: Currently there is no configuration for JVM metrics, so it doesn't matter whether you pass a config or not.
     // However, we will add config options in the future, like whether you want to use Prometheus naming conventions
-    //'or OpenTelemetry semantic conventions for JVM metrics.
+    // or OpenTelemetry semantic conventions for JVM metrics.
     public static Builder builder(PrometheusProperties config) {
         return new Builder(config);
     }
@@ -37,32 +37,32 @@ public class JvmMetrics {
         /**
          * Register all JVM metrics with the default registry.
          * <p>
-         * It's safe to call this multiple times:
-         * Only the first call will register the metrics, all subsequent calls will be ignored.
+         * It's safe to call this multiple times, only the first call will register the metrics, all subsequent calls
+         * will be ignored.
          */
         public void register() {
-            if (!registeredWithTheDefaultRegistry.getAndSet(true)) {
-                register(PrometheusRegistry.defaultRegistry);
-            }
+            register(PrometheusRegistry.defaultRegistry);
         }
 
         /**
          * Register all JVM metrics with the {@code registry}.
          * <p>
-         * You must make sure to call this only once per {@code registry}, otherwise it will
-         * throw an Exception because you are trying to register duplicate metrics.
+         * It's safe to call this multiple times, only the first call will register the metrics, all subsequent calls
+         * will be ignored.
          */
         public void register(PrometheusRegistry registry) {
-            JvmThreadsMetrics.builder(config).register(registry);
-            JvmBufferPoolMetrics.builder(config).register(registry);
-            JvmClassLoadingMetrics.builder(config).register(registry);
-            JvmCompilationMetrics.builder(config).register(registry);
-            JvmGarbageCollectorMetrics.builder(config).register(registry);
-            JvmMemoryPoolAllocationMetrics.builder(config).register(registry);
-            JvmMemoryMetrics.builder(config).register(registry);
-            JvmNativeMemoryMetrics.builder(config).register(registry);
-            JvmRuntimeInfoMetric.builder(config).register(registry);
-            ProcessMetrics.builder(config).register(registry);
+            if (REGISTERED.add(registry)) {
+                JvmThreadsMetrics.builder(config).register(registry);
+                JvmBufferPoolMetrics.builder(config).register(registry);
+                JvmClassLoadingMetrics.builder(config).register(registry);
+                JvmCompilationMetrics.builder(config).register(registry);
+                JvmGarbageCollectorMetrics.builder(config).register(registry);
+                JvmMemoryPoolAllocationMetrics.builder(config).register(registry);
+                JvmMemoryMetrics.builder(config).register(registry);
+                JvmNativeMemoryMetrics.builder(config).register(registry);
+                JvmRuntimeInfoMetric.builder(config).register(registry);
+                ProcessMetrics.builder(config).register(registry);
+            }
         }
     }
 }
