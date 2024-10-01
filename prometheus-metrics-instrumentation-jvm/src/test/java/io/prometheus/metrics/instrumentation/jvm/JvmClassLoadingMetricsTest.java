ee36ad056a4eb7ea96ac4fdae581b8c98e8128ee
Mickael Maison <mickael.maison@gmail.com>
2024-10-01 15:48:04 +0200

    Apply Spotless changes
    Signed-off-by: Mickael Maison <mickael.maison@gmail.com>

--- prometheus-metrics-instrumentation-jvm/src/test/java/io/prometheus/metrics/instrumentation/jvm/JvmClassLoadingMetricsTest.java
+++ prometheus-metrics-instrumentation-jvm/src/test/java/io/prometheus/metrics/instrumentation/jvm/JvmClassLoadingMetricsTest.java
@@ -1,68 +1,62 @@
 package io.prometheus.metrics.instrumentation.jvm;
 
-import io.prometheus.metrics.model.registry.MetricNameFilter;
-import io.prometheus.metrics.model.registry.PrometheusRegistry;
-import io.prometheus.metrics.model.snapshots.MetricSnapshots;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-import org.mockito.Mockito;
-
-import java.io.IOException;
-import java.lang.management.ClassLoadingMXBean;
-
 import static io.prometheus.metrics.instrumentation.jvm.TestUtil.convertToOpenMetricsFormat;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import io.prometheus.metrics.model.registry.MetricNameFilter;
+import io.prometheus.metrics.model.registry.PrometheusRegistry;
+import io.prometheus.metrics.model.snapshots.MetricSnapshots;
+import java.io.IOException;
+import java.lang.management.ClassLoadingMXBean;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
 public class JvmClassLoadingMetricsTest {
 
-    private final ClassLoadingMXBean mockClassLoadingBean = Mockito.mock(ClassLoadingMXBean.class);
+  private final ClassLoadingMXBean mockClassLoadingBean = Mockito.mock(ClassLoadingMXBean.class);
 
-    @Before
-    public void setUp() {
-        when(mockClassLoadingBean.getLoadedClassCount()).thenReturn(1000);
-        when(mockClassLoadingBean.getTotalLoadedClassCount()).thenReturn(2000L);
-        when(mockClassLoadingBean.getUnloadedClassCount()).thenReturn(500L);
-    }
+  @Before
+  public void setUp() {
+    when(mockClassLoadingBean.getLoadedClassCount()).thenReturn(1000);
+    when(mockClassLoadingBean.getTotalLoadedClassCount()).thenReturn(2000L);
+    when(mockClassLoadingBean.getUnloadedClassCount()).thenReturn(500L);
+  }
 
-    @Test
-    public void testGoodCase() throws IOException {
-        PrometheusRegistry registry = new PrometheusRegistry();
-        JvmClassLoadingMetrics.builder()
-                .classLoadingBean(mockClassLoadingBean)
-                .register(registry);
-        MetricSnapshots snapshots = registry.scrape();
+  @Test
+  public void testGoodCase() throws IOException {
+    PrometheusRegistry registry = new PrometheusRegistry();
+    JvmClassLoadingMetrics.builder().classLoadingBean(mockClassLoadingBean).register(registry);
+    MetricSnapshots snapshots = registry.scrape();
 
-        String expected =
-                "# TYPE jvm_classes_currently_loaded gauge\n" +
-                "# HELP jvm_classes_currently_loaded The number of classes that are currently loaded in the JVM\n" +
-                "jvm_classes_currently_loaded 1000.0\n" +
-                "# TYPE jvm_classes_loaded counter\n" +
-                "# HELP jvm_classes_loaded The total number of classes that have been loaded since the JVM has started execution\n" +
-                "jvm_classes_loaded_total 2000.0\n" +
-                "# TYPE jvm_classes_unloaded counter\n" +
-                "# HELP jvm_classes_unloaded The total number of classes that have been unloaded since the JVM has started execution\n" +
-                "jvm_classes_unloaded_total 500.0\n" +
-                "# EOF\n";
+    String expected =
+        "# TYPE jvm_classes_currently_loaded gauge\n"
+            + "# HELP jvm_classes_currently_loaded The number of classes that are currently loaded in the JVM\n"
+            + "jvm_classes_currently_loaded 1000.0\n"
+            + "# TYPE jvm_classes_loaded counter\n"
+            + "# HELP jvm_classes_loaded The total number of classes that have been loaded since the JVM has started execution\n"
+            + "jvm_classes_loaded_total 2000.0\n"
+            + "# TYPE jvm_classes_unloaded counter\n"
+            + "# HELP jvm_classes_unloaded The total number of classes that have been unloaded since the JVM has started execution\n"
+            + "jvm_classes_unloaded_total 500.0\n"
+            + "# EOF\n";
 
-        Assert.assertEquals(expected, convertToOpenMetricsFormat(snapshots));
-    }
+    Assert.assertEquals(expected, convertToOpenMetricsFormat(snapshots));
+  }
 
-    @Test
-    public void testIgnoredMetricNotScraped() {
-        MetricNameFilter filter = MetricNameFilter.builder()
-                .nameMustNotBeEqualTo("jvm_classes_currently_loaded")
-                .build();
+  @Test
+  public void testIgnoredMetricNotScraped() {
+    MetricNameFilter filter =
+        MetricNameFilter.builder().nameMustNotBeEqualTo("jvm_classes_currently_loaded").build();
 
-        PrometheusRegistry registry = new PrometheusRegistry();
-        JvmClassLoadingMetrics.builder()
-                .classLoadingBean(mockClassLoadingBean)
-                .register(registry);
-        registry.scrape(filter);
+    PrometheusRegistry registry = new PrometheusRegistry();
+    JvmClassLoadingMetrics.builder().classLoadingBean(mockClassLoadingBean).register(registry);
+    registry.scrape(filter);
 
-        verify(mockClassLoadingBean, times(0)).getLoadedClassCount();
-        verify(mockClassLoadingBean, times(1)).getTotalLoadedClassCount();
-    }
+    verify(mockClassLoadingBean, times(0)).getLoadedClassCount();
+    verify(mockClassLoadingBean, times(1)).getTotalLoadedClassCount();
+  }
 }
--- prometheus-metrics-instrumentation-jvm/src/test/java/io/prometheus/metrics/instrumentation/jvm/ProcessMetricsTest.java
+++ prometheus-metrics-instrumentation-jvm/src/test/java/io/prometheus/metrics/instrumentation/jvm/ProcessMetricsTest.java
@@ -1,18 +1,5 @@
 package io.prometheus.metrics.instrumentation.jvm;
 
-import io.prometheus.metrics.model.registry.MetricNameFilter;
-import io.prometheus.metrics.model.registry.PrometheusRegistry;
-import io.prometheus.metrics.model.snapshots.MetricSnapshots;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-import org.mockito.Mockito;
-
-import java.io.File;
-import java.io.IOException;
-import java.lang.management.RuntimeMXBean;
-import java.util.concurrent.TimeUnit;
-
 import static io.prometheus.metrics.instrumentation.jvm.TestUtil.convertToOpenMetricsFormat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
@@ -20,104 +7,119 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import io.prometheus.metrics.model.registry.MetricNameFilter;
+import io.prometheus.metrics.model.registry.PrometheusRegistry;
+import io.prometheus.metrics.model.snapshots.MetricSnapshots;
+import java.io.File;
+import java.io.IOException;
+import java.lang.management.RuntimeMXBean;
+import java.util.concurrent.TimeUnit;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
 public class ProcessMetricsTest {
 
-    private final com.sun.management.UnixOperatingSystemMXBean sunOsBean = Mockito.mock(com.sun.management.UnixOperatingSystemMXBean.class);
-    private final java.lang.management.OperatingSystemMXBean javaOsBean = Mockito.mock(java.lang.management.OperatingSystemMXBean.class);
-    private final ProcessMetrics.Grepper linuxGrepper = Mockito.mock(ProcessMetrics.Grepper.class);
-    private final ProcessMetrics.Grepper windowsGrepper = Mockito.mock(ProcessMetrics.Grepper.class);
-    private final RuntimeMXBean runtimeBean = Mockito.mock(RuntimeMXBean.class);
+  private final com.sun.management.UnixOperatingSystemMXBean sunOsBean =
+      Mockito.mock(com.sun.management.UnixOperatingSystemMXBean.class);
+  private final java.lang.management.OperatingSystemMXBean javaOsBean =
+      Mockito.mock(java.lang.management.OperatingSystemMXBean.class);
+  private final ProcessMetrics.Grepper linuxGrepper = Mockito.mock(ProcessMetrics.Grepper.class);
+  private final ProcessMetrics.Grepper windowsGrepper = Mockito.mock(ProcessMetrics.Grepper.class);
+  private final RuntimeMXBean runtimeBean = Mockito.mock(RuntimeMXBean.class);
 
-    @Before
-    public void setUp() throws IOException {
-        when(sunOsBean.getProcessCpuTime()).thenReturn(TimeUnit.MILLISECONDS.toNanos(72));
-        when(sunOsBean.getOpenFileDescriptorCount()).thenReturn(127L);
-        when(sunOsBean.getMaxFileDescriptorCount()).thenReturn(244L);
-        when(runtimeBean.getStartTime()).thenReturn(37100L);
-        when(linuxGrepper.lineStartingWith(any(File.class), eq("VmSize:"))).thenReturn("VmSize:     6036 kB");
-        when(linuxGrepper.lineStartingWith(any(File.class), eq("VmRSS:"))).thenReturn("VmRSS:      1012 kB");
+  @Before
+  public void setUp() throws IOException {
+    when(sunOsBean.getProcessCpuTime()).thenReturn(TimeUnit.MILLISECONDS.toNanos(72));
+    when(sunOsBean.getOpenFileDescriptorCount()).thenReturn(127L);
+    when(sunOsBean.getMaxFileDescriptorCount()).thenReturn(244L);
+    when(runtimeBean.getStartTime()).thenReturn(37100L);
+    when(linuxGrepper.lineStartingWith(any(File.class), eq("VmSize:")))
+        .thenReturn("VmSize:     6036 kB");
+    when(linuxGrepper.lineStartingWith(any(File.class), eq("VmRSS:")))
+        .thenReturn("VmRSS:      1012 kB");
+  }
+
+  @Test
+  public void testGoodCase() throws IOException {
+    PrometheusRegistry registry = new PrometheusRegistry();
+    ProcessMetrics.builder()
+        .osBean(sunOsBean)
+        .runtimeBean(runtimeBean)
+        .grepper(linuxGrepper)
+        .register(registry);
+    MetricSnapshots snapshots = registry.scrape();
+
+    String expected =
+        "# TYPE process_cpu_seconds counter\n"
+            + "# UNIT process_cpu_seconds seconds\n"
+            + "# HELP process_cpu_seconds Total user and system CPU time spent in seconds.\n"
+            + "process_cpu_seconds_total 0.072\n"
+            + "# TYPE process_max_fds gauge\n"
+            + "# HELP process_max_fds Maximum number of open file descriptors.\n"
+            + "process_max_fds 244.0\n"
+            + "# TYPE process_open_fds gauge\n"
+            + "# HELP process_open_fds Number of open file descriptors.\n"
+            + "process_open_fds 127.0\n";
+    if (ProcessMetrics.PROC_SELF_STATUS.canRead()) {
+      expected +=
+          "# TYPE process_resident_memory_bytes gauge\n"
+              + "# UNIT process_resident_memory_bytes bytes\n"
+              + "# HELP process_resident_memory_bytes Resident memory size in bytes.\n"
+              + "process_resident_memory_bytes 1036288.0\n";
     }
-
-    @Test
-    public void testGoodCase() throws IOException {
-        PrometheusRegistry registry = new PrometheusRegistry();
-        ProcessMetrics.builder()
-            .osBean(sunOsBean)
-            .runtimeBean(runtimeBean)
-            .grepper(linuxGrepper)
-            .register(registry);
-        MetricSnapshots snapshots = registry.scrape();
-
-        String expected =
-                "# TYPE process_cpu_seconds counter\n" +
-                "# UNIT process_cpu_seconds seconds\n" +
-                "# HELP process_cpu_seconds Total user and system CPU time spent in seconds.\n" +
-                "process_cpu_seconds_total 0.072\n" +
-                "# TYPE process_max_fds gauge\n" +
-                "# HELP process_max_fds Maximum number of open file descriptors.\n" +
-                "process_max_fds 244.0\n" +
-                "# TYPE process_open_fds gauge\n" +
-                "# HELP process_open_fds Number of open file descriptors.\n" +
-                "process_open_fds 127.0\n";
-        if (ProcessMetrics.PROC_SELF_STATUS.canRead()) {
-            expected +=
-                "# TYPE process_resident_memory_bytes gauge\n" +
-                "# UNIT process_resident_memory_bytes bytes\n" +
-                "# HELP process_resident_memory_bytes Resident memory size in bytes.\n" +
-                "process_resident_memory_bytes 1036288.0\n";
-        }
-        expected +=
-                "# TYPE process_start_time_seconds gauge\n" +
-                "# UNIT process_start_time_seconds seconds\n" +
-                "# HELP process_start_time_seconds Start time of the process since unix epoch in seconds.\n" +
-                "process_start_time_seconds 37.1\n";
-        if (ProcessMetrics.PROC_SELF_STATUS.canRead()) {
-            expected +=
-                "# TYPE process_virtual_memory_bytes gauge\n" +
-                "# UNIT process_virtual_memory_bytes bytes\n" +
-                "# HELP process_virtual_memory_bytes Virtual memory size in bytes.\n" +
-                "process_virtual_memory_bytes 6180864.0\n";
-        }
-        expected += "# EOF\n";
-
-        Assert.assertEquals(expected, convertToOpenMetricsFormat(snapshots));
+    expected +=
+        "# TYPE process_start_time_seconds gauge\n"
+            + "# UNIT process_start_time_seconds seconds\n"
+            + "# HELP process_start_time_seconds Start time of the process since unix epoch in seconds.\n"
+            + "process_start_time_seconds 37.1\n";
+    if (ProcessMetrics.PROC_SELF_STATUS.canRead()) {
+      expected +=
+          "# TYPE process_virtual_memory_bytes gauge\n"
+              + "# UNIT process_virtual_memory_bytes bytes\n"
+              + "# HELP process_virtual_memory_bytes Virtual memory size in bytes.\n"
+              + "process_virtual_memory_bytes 6180864.0\n";
     }
+    expected += "# EOF\n";
 
-    @Test
-    public void testMinimal() throws IOException {
-        PrometheusRegistry registry = new PrometheusRegistry();
-        ProcessMetrics.builder()
-                .osBean(javaOsBean)
-                .runtimeBean(runtimeBean)
-                .grepper(windowsGrepper)
-                .register(registry);
-        MetricSnapshots snapshots = registry.scrape();
+    Assert.assertEquals(expected, convertToOpenMetricsFormat(snapshots));
+  }
 
-        String expected =
-                "# TYPE process_start_time_seconds gauge\n" +
-                "# UNIT process_start_time_seconds seconds\n" +
-                "# HELP process_start_time_seconds Start time of the process since unix epoch in seconds.\n" +
-                "process_start_time_seconds 37.1\n" +
-                "# EOF\n";
+  @Test
+  public void testMinimal() throws IOException {
+    PrometheusRegistry registry = new PrometheusRegistry();
+    ProcessMetrics.builder()
+        .osBean(javaOsBean)
+        .runtimeBean(runtimeBean)
+        .grepper(windowsGrepper)
+        .register(registry);
+    MetricSnapshots snapshots = registry.scrape();
 
-        Assert.assertEquals(expected, convertToOpenMetricsFormat(snapshots));
-    }
+    String expected =
+        "# TYPE process_start_time_seconds gauge\n"
+            + "# UNIT process_start_time_seconds seconds\n"
+            + "# HELP process_start_time_seconds Start time of the process since unix epoch in seconds.\n"
+            + "process_start_time_seconds 37.1\n"
+            + "# EOF\n";
 
-    @Test
-    public void testIgnoredMetricNotScraped() {
-        MetricNameFilter filter = MetricNameFilter.builder()
-                .nameMustNotBeEqualTo("process_max_fds")
-                .build();
+    Assert.assertEquals(expected, convertToOpenMetricsFormat(snapshots));
+  }
 
-        PrometheusRegistry registry = new PrometheusRegistry();
-        ProcessMetrics.builder()
-                .osBean(sunOsBean)
-                .runtimeBean(runtimeBean)
-                .grepper(linuxGrepper)
-                .register(registry);
-        registry.scrape(filter);
+  @Test
+  public void testIgnoredMetricNotScraped() {
+    MetricNameFilter filter =
+        MetricNameFilter.builder().nameMustNotBeEqualTo("process_max_fds").build();
 
-        verify(sunOsBean, times(0)).getMaxFileDescriptorCount();
-        verify(sunOsBean, times(1)).getOpenFileDescriptorCount();
-    }
+    PrometheusRegistry registry = new PrometheusRegistry();
+    ProcessMetrics.builder()
+        .osBean(sunOsBean)
+        .runtimeBean(runtimeBean)
+        .grepper(linuxGrepper)
+        .register(registry);
+    registry.scrape(filter);
+
+    verify(sunOsBean, times(0)).getMaxFileDescriptorCount();
+    verify(sunOsBean, times(1)).getOpenFileDescriptorCount();
+  }
 }
