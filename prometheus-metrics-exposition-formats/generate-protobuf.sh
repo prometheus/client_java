4b0b105b95ad6a5db3c309d5ee9dacc667228c06
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2024-10-08 12:49:18 +0200

    fix shading
    Signed-off-by: Gregor Zeitlinger <gregor.zeitlinger@grafana.com>

--- prometheus-metrics-exposition-formats/generate-protobuf.sh
+++ prometheus-metrics-exposition-formats/generate-protobuf.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-set -euo pipefail
+set -euxo pipefail
 
 # We use the shaded protobuf JAR from the protobuf-shaded module.
 # I could not figure out how to use a protoc Maven plugin to use the shaded module, so I ran this command to generate the sources manually.
 
 TARGET_DIR=$1
+PROTO_DIR=src/main/protobuf
 PROTOBUF_VERSION_STRING=$2
 
 echo "Generating protobuf sources for version $PROTOBUF_VERSION_STRING in $TARGET_DIR"
 
-rm -rf src/main/protobuf/*
-curl -sL https://raw.githubusercontent.com/prometheus/client_model/master/io/prometheus/client/metrics.proto -o src/main/protobuf/metrics.proto
-sed -i "s/java_package = \"io.prometheus.client\"/java_package = \"io.prometheus.metrics.expositionformats.generated.com_google_protobuf_${PROTOBUF_VERSION_STRING}\"/" src/main/protobuf/metrics.proto
-rm -rf $TARGET_DIR/*
-protoc --java_out $TARGET_DIR src/main/protobuf/metrics.proto
+rm -rf TARGET_DIR/*
+rm -rf $PROTO_DIR/*
+
+curl -sL https://raw.githubusercontent.com/prometheus/client_model/master/io/prometheus/client/metrics.proto -o $PROTO_DIR/metrics.proto
+sed -i "s/java_package = \"io.prometheus.client\"/java_package = \"io.prometheus.metrics.expositionformats.generated.com_google_protobuf_${PROTOBUF_VERSION_STRING}\"/" $PROTO_DIR/metrics.proto
+protoc --java_out $TARGET_DIR $PROTO_DIR/metrics.proto
 
