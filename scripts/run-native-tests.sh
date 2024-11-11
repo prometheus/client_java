95378f26efca793fe33d1670d948f35b61a597af
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2024-11-11 09:55:22 +0100

    smoke test
    Signed-off-by: Gregor Zeitlinger <gregor.zeitlinger@grafana.com>

--- scripts/run-native-tests.sh
+++ scripts/run-native-tests.sh
@@ -3,12 +3,5 @@
 set -euo pipefail
 
 ./mvnw clean install -DskipTests
-
-PROJECT_VERSION=$(sed -nE 's/<version>(.*)<\/version>/\1/p' pom.xml | head -1 | xargs)
-
 cd integration-tests/it-spring-boot-smoke-test
-
-# replace the version in the pom.xml
-sed -i "s/<version>0.1<\/version>/<version>$PROJECT_VERSION<\/version>/" pom.xml
-
 ../../mvnw  test -PnativeTest
