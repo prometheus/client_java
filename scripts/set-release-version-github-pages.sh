8bc1f0292b5146219d67e17d967a96448da233f7
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2024-11-11 18:13:53 +0100

    set latest release
    Signed-off-by: Gregor Zeitlinger <gregor.zeitlinger@grafana.com>

--- scripts/set-release-version-github-pages.sh
+++ scripts/set-release-version-github-pages.sh
@@ -2,6 +2,6 @@
 
 set -euo pipefail
 
-version=$(git tag -l | grep 'v' | sort | tail -1)
+version=$(git tag -l | grep 'v' | sort | tail -1 | sed 's/v//')
 marker="\$version"
 sed -i "s/$marker/$version/g" docs/content/getting-started/quickstart.md
