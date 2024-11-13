cafbc1474c681705a7b0700789046112cab3c85c
Petar Heyken <48122009+pheyken@users.noreply.github.com>
2024-11-13 12:12:37 +0100

    fix register part of documentation for caffeine & guava instrumentation + fix typo in method name (#1203)
    Signed-off-by: Petar Heyken <mail@petar-heyken.de>

--- prometheus-metrics-instrumentation-guava/src/main/java/io/prometheus/metrics/instrumentation/guava/CacheMetricsCollector.java
+++ prometheus-metrics-instrumentation-guava/src/main/java/io/prometheus/metrics/instrumentation/guava/CacheMetricsCollector.java
@@ -23,7 +23,8 @@ import java.util.concurrent.ConcurrentMap;
  * <pre>{@code
  * // Note that `recordStats()` is required to gather non-zero statistics
  * Cache<String, String> cache = CacheBuilder.newBuilder().recordStats().build();
- * CacheMetricsCollector cacheMetrics = new CacheMetricsCollector().register();
+ * CacheMetricsCollector cacheMetrics = new CacheMetricsCollector();
+ * PrometheusRegistry.defaultRegistry.register(cacheMetrics);
  * cacheMetrics.addCache("mycache", cache);
  *
  * }</pre>
