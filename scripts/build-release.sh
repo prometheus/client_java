7a7951387b5417cbcd61d38265075db624309c1d
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2024-11-25 17:15:26 +0100

    fix release
    Signed-off-by: Gregor Zeitlinger <gregor.zeitlinger@grafana.com>

--- scripts/build-release.sh
+++ scripts/build-release.sh
@@ -6,7 +6,6 @@ TAG=$1
 VERSION=${TAG#v}
 
 mvn versions:set -DnewVersion=$VERSION
-mvn install -DskipTests=true # to find the new version in the next step
+mvn -B install -P release -DskipTests=true # to find the new version in the next step
 cd integration-tests/it-spring-boot-smoke-test
 mvn versions:set -DnewVersion=$VERSION
-mvn -B package -P release -Dmaven.test.skip=true
