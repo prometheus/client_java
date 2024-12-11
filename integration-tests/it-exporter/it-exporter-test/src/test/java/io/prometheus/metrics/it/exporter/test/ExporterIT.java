20ffaf48d51c4d4f0bacaa009b404a22066bb172
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2024-12-11 17:43:47 +0100

    update protobuf
    Signed-off-by: Gregor Zeitlinger <gregor.zeitlinger@grafana.com>

--- integration-tests/it-exporter/it-exporter-test/src/test/java/io/prometheus/metrics/it/exporter/test/ExporterIT.java
+++ integration-tests/it-exporter/it-exporter-test/src/test/java/io/prometheus/metrics/it/exporter/test/ExporterIT.java
@@ -5,7 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import com.google.common.io.Resources;
 import io.prometheus.client.it.common.ExporterTest;
-import io.prometheus.metrics.expositionformats.generated.com_google_protobuf_4_28_3.Metrics;
+import io.prometheus.metrics.expositionformats.generated.com_google_protobuf_4_29_1.Metrics;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URLEncoder;
