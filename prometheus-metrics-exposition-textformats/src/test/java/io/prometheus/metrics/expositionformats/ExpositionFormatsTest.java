ddd17b16e44ce2900f8b8115c09c5252a3bed6cf
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-02-18 12:12:13 +0100

    increase coverage
    Signed-off-by: Gregor Zeitlinger <gregor.zeitlinger@grafana.com>

--- prometheus-metrics-exposition-textformats/src/test/java/io/prometheus/metrics/expositionformats/ExpositionFormatsTest.java
+++ prometheus-metrics-exposition-textformats/src/test/java/io/prometheus/metrics/expositionformats/ExpositionFormatsTest.java
@@ -2,9 +2,6 @@ package io.prometheus.metrics.expositionformats;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import io.prometheus.metrics.expositionformats.generated.com_google_protobuf_4_29_3.Metrics;
-import io.prometheus.metrics.expositionformats.internal.PrometheusProtobufWriterImpl;
-import io.prometheus.metrics.expositionformats.internal.ProtobufUtil;
 import io.prometheus.metrics.model.snapshots.ClassicHistogramBuckets;
 import io.prometheus.metrics.model.snapshots.CounterSnapshot;
 import io.prometheus.metrics.model.snapshots.CounterSnapshot.CounterDataPointSnapshot;
@@ -2659,10 +2656,5 @@ class ExpositionFormatsTest {
     assertThat(out).hasToString(expected);
   }
 
-  private void assertPrometheusProtobuf(String expected, MetricSnapshot snapshot) {
-    PrometheusProtobufWriterImpl writer = new PrometheusProtobufWriterImpl();
-    Metrics.MetricFamily protobufData = writer.convert(snapshot);
-    String actual = ProtobufUtil.shortDebugString(protobufData);
-    assertThat(actual).isEqualTo(expected);
-  }
+  protected void assertPrometheusProtobuf(String expected, MetricSnapshot snapshot) {}
 }
