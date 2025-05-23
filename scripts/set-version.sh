79ad1dc9f7d3269b172ecaf97a96e9204865b45a
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-05-23 10:41:59 +0200

    set version
    Signed-off-by: Gregor Zeitlinger <gregor.zeitlinger@grafana.com>

--- scripts/set-version.sh
+++ scripts/set-version.sh
@@ -12,9 +12,7 @@ newVersion=$1
 grep "\<module\>" pom.xml | sed 's/<\/module>//g' | sed 's/.*<module>//g' | sed 's/.*\///g' | while IFS= read -r module
 do
     # set the version of the module
-    # and update reference to this module into others modules
-    mvn versions:set -DgenerateBackupPoms=false -DartifactId="$module" \
-        -DnewVersion="$newVersion" -DupdateMatchingVersions=true
+    mvn versions:set -DgenerateBackupPoms=false -DartifactId="$module" -DnewVersion="$newVersion"
 done
 # set the version of the aggregator pom
 mvn versions:set -DnewVersion="$newVersion" -DgenerateBackupPoms=false
