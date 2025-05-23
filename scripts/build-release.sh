9e652fa6c44243c76a36e5f60fbded64b36ca41b
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-05-23 10:46:46 +0200

    set version
    Signed-off-by: Gregor Zeitlinger <gregor.zeitlinger@grafana.com>

--- scripts/build-release.sh
+++ scripts/build-release.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 
 VERSION=${TAG#v}
 
-mvn versions:set -P setVersion -DnewVersion="$VERSION"
+./setVersion "$VERSION"
 mvn -B package -P release -Dmaven.test.skip=true
--- scripts/set-version.sh
+++ scripts/set-version.sh
@@ -8,11 +8,10 @@ if [ $# -ne 1 ]; then
 fi
 newVersion=$1
 
-# for each module into aggregator pom
-grep "\<module\>" pom.xml | sed 's/<\/module>//g' | sed 's/.*<module>//g' | sed 's/.*\///g' | while IFS= read -r module
-do
-    # set the version of the module
-    mvn versions:set -DgenerateBackupPoms=false -DartifactId="$module" -DnewVersion="$newVersion"
-done
-# set the version of the aggregator pom
 mvn versions:set -DnewVersion="$newVersion" -DgenerateBackupPoms=false
+pushd prometheus-metrics-parent
+mvn versions:set -DnewVersion="$newVersion" -DgenerateBackupPoms=false
+popd
+pushd prometheus-metrics-bom
+mvn versions:set -DnewVersion="$newVersion" -DgenerateBackupPoms=false
+popd
