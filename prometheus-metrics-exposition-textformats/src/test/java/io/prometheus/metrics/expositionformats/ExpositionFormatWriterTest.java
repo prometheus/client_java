cca01f6ee2ee7560d81107dccc968c1eacc924d5
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-08-08 19:52:16 +0200

    coverage
    Signed-off-by: Gregor Zeitlinger <gregor.zeitlinger@grafana.com>

--- prometheus-metrics-exposition-textformats/src/test/java/io/prometheus/metrics/expositionformats/ExpositionFormatWriterTest.java
+++ prometheus-metrics-exposition-textformats/src/test/java/io/prometheus/metrics/expositionformats/ExpositionFormatWriterTest.java
@@ -4,6 +4,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import io.prometheus.metrics.model.snapshots.EscapingScheme;
 import io.prometheus.metrics.model.snapshots.MetricSnapshots;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import org.junit.jupiter.api.Test;
 
 class ExpositionFormatWriterTest {
@@ -11,9 +13,22 @@ class ExpositionFormatWriterTest {
   private final ExpositionFormatWriter writer = OpenMetricsTextFormatWriter.create();
 
   @Test
+  void write() throws IOException {
+    MetricSnapshots snapshots = new MetricSnapshots();
+    ByteArrayOutputStream out = new ByteArrayOutputStream();
+    writer.write(out, snapshots, EscapingScheme.NO_ESCAPING);
+    assertThat(out).hasToString("# EOF\n");
+
+    out.reset();
+    writer.write(out, snapshots);
+    assertThat(out).hasToString("# EOF\n");
+  }
+
+  @Test
   void toDebugString() {
     assertThat(writer.toDebugString(new MetricSnapshots(), EscapingScheme.NO_ESCAPING))
         .isEqualTo("# EOF\n");
+    assertThat(writer.toDebugString(new MetricSnapshots())).isEqualTo("# EOF\n");
   }
 
   @Test
