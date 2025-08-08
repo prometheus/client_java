cddd8296d931b0267e4515ea41156e2cd046fe59
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-08-08 19:51:10 +0200

    ensure binary compatibility
    Signed-off-by: Gregor Zeitlinger <gregor.zeitlinger@grafana.com>

--- prometheus-metrics-exposition-textformats/src/main/java/io/prometheus/metrics/expositionformats/ExpositionFormatWriter.java
+++ prometheus-metrics-exposition-textformats/src/main/java/io/prometheus/metrics/expositionformats/ExpositionFormatWriter.java
@@ -2,6 +2,7 @@ package io.prometheus.metrics.expositionformats;
 
 import io.prometheus.metrics.model.snapshots.EscapingScheme;
 import io.prometheus.metrics.model.snapshots.MetricSnapshots;
+import io.prometheus.metrics.model.snapshots.PrometheusNaming;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
@@ -9,10 +10,16 @@ import java.io.OutputStream;
 public interface ExpositionFormatWriter {
   boolean accepts(String acceptHeader);
 
-  /** Text formats use UTF-8 encoding. */
+  /** Writes the given metric snapshots to the output stream using the specified escaping scheme. */
   void write(OutputStream out, MetricSnapshots metricSnapshots, EscapingScheme escapingScheme)
       throws IOException;
 
+  /** Writes the given metric snapshots to the output stream using the default escaping scheme. */
+  default void write(OutputStream out, MetricSnapshots metricSnapshots) throws IOException {
+    write(out, metricSnapshots, PrometheusNaming.DEFAULT_ESCAPING_SCHEME);
+  }
+
+  /** Converts the metric snapshots to a debug string using the specified escaping scheme. */
   default String toDebugString(MetricSnapshots metricSnapshots, EscapingScheme escapingScheme) {
     ByteArrayOutputStream out = new ByteArrayOutputStream();
     try {
@@ -21,6 +28,11 @@ public interface ExpositionFormatWriter {
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
+  }
+
+  /** Converts the metric snapshots to a debug string using the default escaping scheme. */
+  default String toDebugString(MetricSnapshots metricSnapshots) {
+    return toDebugString(metricSnapshots, PrometheusNaming.DEFAULT_ESCAPING_SCHEME);
   }
 
   String getContentType();
