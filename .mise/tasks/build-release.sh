e21400b1c2e7c6dd52929b00d366ffe67653a493
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-11-04 16:09:37 +0100

    cleanup mise and use JDK 25 for release build as well
    Signed-off-by: Gregor Zeitlinger <gregor.zeitlinger@grafana.com>

--- .mise/tasks/build-release.sh
+++ .mise/tasks/build-release.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 #MISE description="Build release package"
-#MISE env={TAG = "1.5.0-SNAPSHOT"}
+#MISE arg "<tag>" env="TAG" default="1.5.0-SNAPSHOT"
 
 set -euo pipefail
 
-VERSION=${TAG#v}
+VERSION=${usage_tag#v}
 
-./scripts/set-version.sh "$VERSION"
+mise run set-version "$VERSION"
 mvn -B package -P 'release,!default' -Dmaven.test.skip=true
