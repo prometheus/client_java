c963d4aa5c8a0441a16e0f2fe93a71efb4f3de3b
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-11-04 16:27:32 +0100

    format
    Signed-off-by: Gregor Zeitlinger <gregor.zeitlinger@grafana.com>

--- .mise/tasks/set-version.sh
+++ .mise/tasks/set-version.sh
@@ -5,8 +5,9 @@
 
 set -euo pipefail
 
-# replace all occurrences '<version>1.5.0-SNAPSHOT</version>' with '<version>$usage_version</version>'
-# in all pom.xml files in the current directory and subdirectories
+# replace all occurrences '<version>1.5.0-SNAPSHOT</version>' with
+# '<version>$usage_version</version>' in all pom.xml files in the current directory and
+# subdirectories
 
 find . -name 'pom.xml' -exec \
 	sed -i "s/<version>1.5.0-SNAPSHOT<\/version>/<version>$usage_version<\/version>/g" {} +
