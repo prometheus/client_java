cfb1bdad3c3433ca9b2f8a7092cb02a491a81967
Igor Melnichenko <myllyenko@ya.ru>
2025-11-20 19:02:36 +0300

    Adding ability to disable compression in PrometheusScrapeHandler (#1691)
    Resolves #1684
    
    ---------
    
    Signed-off-by: Igor Melnichenko <i-melnichenko@yandex-team.ru>

--- prometheus-metrics-config/src/test/java/io/prometheus/metrics/config/ExporterHttpServerPropertiesTest.java
+++ prometheus-metrics-config/src/test/java/io/prometheus/metrics/config/ExporterHttpServerPropertiesTest.java
@@ -2,6 +2,7 @@ package io.prometheus.metrics.config;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -13,6 +14,7 @@ class ExporterHttpServerPropertiesTest {
     ExporterHttpServerProperties properties =
         load(Map.of("io.prometheus.exporter.httpServer.port", "1"));
     assertThat(properties.getPort()).isOne();
+    assertThat(properties.isPreferUncompressedResponse()).isFalse();
 
     assertThatExceptionOfType(PrometheusPropertiesException.class)
         .isThrownBy(() -> load(Map.of("io.prometheus.exporter.httpServer.port", "0")))
@@ -21,7 +23,14 @@ class ExporterHttpServerPropertiesTest {
 
   @Test
   void builder() {
-    assertThat(ExporterHttpServerProperties.builder().port(1).build().getPort()).isOne();
+    ExporterHttpServerProperties properties =
+        ExporterHttpServerProperties.builder().port(1).build();
+
+    assertSoftly(
+        softly -> {
+          softly.assertThat(properties.getPort()).isOne();
+          softly.assertThat(properties.isPreferUncompressedResponse()).isFalse();
+        });
   }
 
   private static ExporterHttpServerProperties load(Map<String, String> map) {
