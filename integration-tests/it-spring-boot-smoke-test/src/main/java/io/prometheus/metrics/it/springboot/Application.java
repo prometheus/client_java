d56f6c6e7178520754592aad870d28960cbc7af0
Jay DeLuca <jaydeluca4@gmail.com>
2025-12-09 09:21:47 -0500

    more lint errors
    Signed-off-by: Jay DeLuca <jaydeluca4@gmail.com>

--- integration-tests/it-spring-boot-smoke-test/src/main/java/io/prometheus/metrics/it/springboot/Application.java
+++ integration-tests/it-spring-boot-smoke-test/src/main/java/io/prometheus/metrics/it/springboot/Application.java
@@ -4,9 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public final class Application {
 
-  public static void main(String[] args) {
+  private Application() {
+    // Utility class
+  }
+
+  public static void main(final String[] args) {
     SpringApplication.run(Application.class, args);
   }
 }
