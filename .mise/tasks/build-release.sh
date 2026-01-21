c6b6d5a3b49e3836d0227cd8399e9fbe85e5ac33
Jay DeLuca <jaydeluca4@gmail.com>
2026-01-21 07:47:56 -0500

    retry other java versions
    Signed-off-by: Jay DeLuca <jaydeluca4@gmail.com>

--- .mise/tasks/build-release.sh
+++ .mise/tasks/build-release.sh
@@ -9,7 +9,4 @@ set -euo pipefail
 VERSION=${usage_tag#v}
 
 mise run set-version "$VERSION"
-mvn -B package \
-  -P 'release,!default,!examples-and-integration-tests' \
-  -Dmaven.test.skip=true \
-  -Dgpg.skip=true
+mvn -B package -P 'release,!default,!examples-and-integration-tests' -Dmaven.test.skip=true -Dgpg.skip=true
