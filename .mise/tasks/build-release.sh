e5c129ebdb46eb610a43105fe955471f5ec42145
Jay DeLuca <jaydeluca4@gmail.com>
2026-01-21 06:35:46 -0500

    exclude others from release build
    Signed-off-by: Jay DeLuca <jaydeluca4@gmail.com>

--- .mise/tasks/build-release.sh
+++ .mise/tasks/build-release.sh
@@ -9,4 +9,4 @@ set -euo pipefail
 VERSION=${usage_tag#v}
 
 mise run set-version "$VERSION"
-mvn -B install -P 'release,!default' -DskipTests -Dgpg.skip=true
+mvn -B package -P 'release,!default,!examples-and-integration-tests' -Dmaven.test.skip=true -Dgpg.skip=true
