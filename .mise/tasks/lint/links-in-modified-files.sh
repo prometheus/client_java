defbb1c61c0bdbd0b9c0f08fe008d2e45bd3645e
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2026-01-26 13:54:13 +0100

    lint only changed text files
    Signed-off-by: Gregor Zeitlinger <gregor.zeitlinger@grafana.com>

--- .mise/tasks/lint/links-in-modified-files.sh
+++ .mise/tasks/lint/links-in-modified-files.sh
@@ -6,12 +6,14 @@ set -e
 #USAGE flag "--base <base>" help="base branch to compare against (default: origin/main)" default="origin/main"
 #USAGE flag "--head <head>" help="head branch to compare against (empty for local changes) (default: empty)" default=""
 
+# shellcheck disable=SC2154
 if [ "$usage_head" = "''" ]; then
 	usage_head=""
 fi
 
 # Check if lychee config was modified
 # - because usage_head may be empty
+# shellcheck disable=SC2086,SC2154
 config_modified=$(git diff --name-only --merge-base "$usage_base" $usage_head |
 	grep -E '^(\.github/config/lychee\.toml|\.mise/tasks/lint/.*|mise\.toml)$' || true)
 
@@ -22,6 +24,7 @@ else
 	# Using lychee's default extension filter here to match when it runs against all files
 	# Note: --diff-filter=d filters out deleted files
 	# - because usage_head may be empty
+	# shellcheck disable=SC2086
 	modified_files=$(git diff --name-only --diff-filter=d "$usage_base" $usage_head |
 		grep -E '\.(md|mkd|mdx|mdown|mdwn|mkdn|mkdown|markdown|html|htm|txt)$' |
 		tr '\n' ' ' || true)
@@ -31,5 +34,6 @@ else
 		exit 0
 	fi
 
+	# shellcheck disable=SC2086
 	mise run lint:links $modified_files
 fi
--- .mise/tasks/lint/links.sh
+++ .mise/tasks/lint/links.sh
@@ -5,8 +5,10 @@ set -e
 
 #USAGE arg "<file>" var=#true help="files to check" default="."
 
+# shellcheck disable=SC2154
 for f in $usage_file; do
 	echo "Checking links in file: $f"
 done
 
+# shellcheck disable=SC2086
 lychee --verbose --config .github/config/lychee.toml $usage_file
