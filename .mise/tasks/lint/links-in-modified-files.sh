0c17a600e18b16c674b98873f54d77b4c92b2f12
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2026-01-26 13:21:38 +0100

    lint only changed text files
    Signed-off-by: Gregor Zeitlinger <gregor.zeitlinger@grafana.com>

--- .mise/tasks/lint/links-in-modified-files.sh
+++ .mise/tasks/lint/links-in-modified-files.sh
@@ -7,29 +7,29 @@ set -e
 #USAGE flag "--head <head>" help="head branch to compare against (empty for local changes) (default: empty)" default=""
 
 if [ "$usage_head" = "''" ]; then
-  usage_head=""
+	usage_head=""
 fi
 
 # Check if lychee config was modified
 # - because usage_head may be empty
-config_modified=$(git diff --name-only --merge-base "$usage_base" $usage_head \
-                  | grep -E '^(\.github/config/lychee\.toml|\.mise/tasks/lint/.*|mise\.toml)$' || true)
+config_modified=$(git diff --name-only --merge-base "$usage_base" $usage_head |
+	grep -E '^(\.github/config/lychee\.toml|\.mise/tasks/lint/.*|mise\.toml)$' || true)
 
-if [ -n "$config_modified" ] ; then
-  echo "config changes, checking all files."
-  mise run lint:links
+if [ -n "$config_modified" ]; then
+	echo "config changes, checking all files."
+	mise run lint:links
 else
-  # Using lychee's default extension filter here to match when it runs against all files
-  # Note: --diff-filter=d filters out deleted files
-  # - because usage_head may be empty
-  modified_files=$(git diff --name-only --diff-filter=d "$usage_base" $usage_head \
-                    | grep -E '\.(md|mkd|mdx|mdown|mdwn|mkdn|mkdown|markdown|html|htm|txt)$' \
-                    | tr '\n' ' ' || true)
+	# Using lychee's default extension filter here to match when it runs against all files
+	# Note: --diff-filter=d filters out deleted files
+	# - because usage_head may be empty
+	modified_files=$(git diff --name-only --diff-filter=d "$usage_base" $usage_head |
+		grep -E '\.(md|mkd|mdx|mdown|mdwn|mkdn|mkdown|markdown|html|htm|txt)$' |
+		tr '\n' ' ' || true)
 
-  if [ -z "$modified_files" ]; then
-    echo "No modified files, skipping link linting."
-    exit 0
-  fi
+	if [ -z "$modified_files" ]; then
+		echo "No modified files, skipping link linting."
+		exit 0
+	fi
 
-  mise run lint:links $modified_files
+	mise run lint:links $modified_files
 fi
--- .mise/tasks/lint/local-links.sh
+++ .mise/tasks/lint/local-links.sh
@@ -6,7 +6,7 @@ set -e
 #USAGE arg "<file>" var=#true help="files to check" default="."
 
 for f in $usage_file; do
-    echo "Checking links in file: $f"
+	echo "Checking links in file: $f"
 done
 
 lychee --verbose --scheme file --include-fragments --config .github/config/lychee.toml $usage_file
