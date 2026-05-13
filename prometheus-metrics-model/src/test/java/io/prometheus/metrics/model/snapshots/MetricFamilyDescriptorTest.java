64933d2580124cf30f6c30eddc2d4898f42a144c
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2026-05-13 12:25:11 +0000

    test: cover typed family descriptor variants Signed-off-by: Gregor Zeitlinger <gregor.zeitlinger@grafana.com>

--- prometheus-metrics-model/src/test/java/io/prometheus/metrics/model/snapshots/MetricFamilyDescriptorTest.java
+++ prometheus-metrics-model/src/test/java/io/prometheus/metrics/model/snapshots/MetricFamilyDescriptorTest.java
@@ -50,13 +50,73 @@ class MetricFamilyDescriptorTest {
   }
 
   @Test
-  void genericFactoryUsesTypedBuilderSemantics() {
+  void histogramDescriptorKeepsLiteralName() {
+    MetricFamilyDescriptor descriptor =
+        MetricFamilyDescriptor.histogram("request_duration_seconds")
+            .help("Request duration")
+            .labelName("method")
+            .build();
+
+    assertThat(descriptor.getType()).isEqualTo(MetricType.HISTOGRAM);
+    assertThat(descriptor.getPrometheusName()).isEqualTo("request_duration_seconds");
+    assertThat(descriptor.getLabelNames()).containsExactly("method");
+  }
+
+  @Test
+  void summaryDescriptorKeepsLiteralName() {
+    MetricFamilyDescriptor descriptor =
+        MetricFamilyDescriptor.summary("request_size_bytes")
+            .help("Request size")
+            .labelName("method")
+            .build();
+
+    assertThat(descriptor.getType()).isEqualTo(MetricType.SUMMARY);
+    assertThat(descriptor.getPrometheusName()).isEqualTo("request_size_bytes");
+    assertThat(descriptor.getLabelNames()).containsExactly("method");
+  }
+
+  @Test
+  void stateSetDescriptorKeepsLiteralName() {
+    MetricFamilyDescriptor descriptor =
+        MetricFamilyDescriptor.stateSet("feature_flags").help("Flags").labelName("service").build();
+
+    assertThat(descriptor.getType()).isEqualTo(MetricType.STATESET);
+    assertThat(descriptor.getPrometheusName()).isEqualTo("feature_flags");
+    assertThat(descriptor.getLabelNames()).containsExactly("service");
+  }
+
+  @Test
+  void unknownDescriptorKeepsLiteralName() {
+    MetricFamilyDescriptor descriptor =
+        MetricFamilyDescriptor.unknown("vendor_metric").help("Vendor metric").build();
+
+    assertThat(descriptor.getType()).isEqualTo(MetricType.UNKNOWN);
+    assertThat(descriptor.getPrometheusName()).isEqualTo("vendor_metric");
+  }
+
+  @Test
+  void genericFactoryUsesTypedBuilderSemanticsForAllKinds() {
     MetricFamilyDescriptor counter =
         MetricFamilyDescriptor.of(MetricType.COUNTER, "http_requests_total").build();
+    MetricFamilyDescriptor gauge =
+        MetricFamilyDescriptor.of(MetricType.GAUGE, "queue_depth").build();
+    MetricFamilyDescriptor histogram =
+        MetricFamilyDescriptor.of(MetricType.HISTOGRAM, "request_duration_seconds").build();
+    MetricFamilyDescriptor summary =
+        MetricFamilyDescriptor.of(MetricType.SUMMARY, "request_size_bytes").build();
     MetricFamilyDescriptor info = MetricFamilyDescriptor.of(MetricType.INFO, "build_info").build();
+    MetricFamilyDescriptor stateSet =
+        MetricFamilyDescriptor.of(MetricType.STATESET, "feature_flags").build();
+    MetricFamilyDescriptor unknown =
+        MetricFamilyDescriptor.of(MetricType.UNKNOWN, "vendor_metric").build();
 
     assertThat(counter.getPrometheusName()).isEqualTo("http_requests");
+    assertThat(gauge.getPrometheusName()).isEqualTo("queue_depth");
+    assertThat(histogram.getPrometheusName()).isEqualTo("request_duration_seconds");
+    assertThat(summary.getPrometheusName()).isEqualTo("request_size_bytes");
     assertThat(info.getPrometheusName()).isEqualTo("build");
+    assertThat(stateSet.getPrometheusName()).isEqualTo("feature_flags");
+    assertThat(unknown.getPrometheusName()).isEqualTo("vendor_metric");
   }
 
   @Test
