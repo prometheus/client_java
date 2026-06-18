84259909e0a7c48bafa3faf25b81beb743b33e5b
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2026-06-18 06:35:04 +0000

    Use Prometheus custom checks for OATS acceptance Signed-off-by: Gregor Zeitlinger <gregor.zeitlinger@grafana.com>

---
+++ examples/example-exporter-opentelemetry/oats-tests/http/check_uptime_metric.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import json
+from urllib.request import urlopen
+
+def get_json(url):
+    with urlopen(url) as response:
+        return json.loads(response.read().decode("utf-8"))
+
+res = get_json("http://localhost:9090/api/v1/query?query=uptime_seconds_total")
+results = res["data"]["result"]
+assert results, "Expected uptime_seconds_total to be present"
