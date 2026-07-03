775ef5493af82b23a8aa636919f809d2d08ee555
Arnab Nandy <arnab_nandy7@yahoo.com>
2026-07-03 18:14:43 +0530

    Fix native histogram bucket index merge on scale-down
    Signed-off-by: Arnab Nandy <arnab_nandy7@yahoo.com>

--- prometheus-metrics-core/src/main/java/io/prometheus/metrics/core/metrics/Histogram.java
+++ prometheus-metrics-core/src/main/java/io/prometheus/metrics/core/metrics/Histogram.java
@@ -614,7 +614,7 @@ public class Histogram extends StatefulMetric<DistributionDataPoint, Histogram.D
       }
       buckets.clear();
       for (i = 0; i < keys.length; i++) {
-        int index = (keys[i] + 1) / 2;
+        int index = (keys[i] > 0 ? keys[i] + 1 : keys[i]) / 2;
         LongAdder count = buckets.computeIfAbsent(index, k -> new LongAdder());
         count.add(values[i]);
       }
